8a35a33f7b612f136f7e4cc4a57ab08c2130eed6
rizzzse <rizzzse@gmail.com>
2022-02-19 01:18:00 +0900

    css関係のなんやかんや

--- renderer/vite.config.ts
+++ renderer/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import linaria from "@linaria/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     root: __dirname,
     base: "./",
-    plugins: [react()],
+    plugins: [
+        react(),
+        linaria({ include: ["**/*.tsx"] }),
+    ],
     build: {
         outDir: "../app",
         polyfillModulePreload: false,
