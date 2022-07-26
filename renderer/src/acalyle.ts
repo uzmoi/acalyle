e0453f68128147fea80bd0f9b242cf5dca7027f5
rizzzse <rizzzse@gmail.com>
2022-07-26 17:57:00 +0900

    refactor

--- renderer/src/acalyle.ts
+++ renderer/src/acalyle.ts
@@ -18,24 +18,25 @@ if(import.meta.vitest) {
     };
 }
 
-export const relayEnv = new Environment({
-    network: Network.create(async (params, variables): Promise<GraphQLResponse> => {
-        if(params.text) {
-            const result = await ipc.graphql(params.text, variables);
-            return {
-                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                data: result.data || undefined!,
-                errors: result.errors?.map(err => ({
-                    message: err.message,
-                    locations: err.locations?.slice(),
-                })),
-                extensions: result.extensions,
-            };
-        }
-        return Promise.reject();
-    }),
-    store: new Store(new RecordSource()),
+const network = Network.create(async (params, variables): Promise<GraphQLResponse> => {
+    if(params.text) {
+        const result = await ipc.graphql(params.text, variables);
+        return {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            data: result.data || undefined!,
+            errors: result.errors?.map(err => ({
+                message: err.message,
+                locations: err.locations?.slice(),
+            })),
+            extensions: result.extensions,
+        };
+    }
+    return Promise.reject();
 });
+
+const store = new Store(new RecordSource());
+
+export const relayEnv = new Environment({ network, store });
 
 export const acalyle = {
     cwd: ipc.cwd,
