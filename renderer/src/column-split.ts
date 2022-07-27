de35e153033d15e4474b10468816a3f6314a5acf
rizzzse <rizzzse@gmail.com>
2022-07-27 19:40:12 +0900

    add test for columnSplit

--- renderer/src/column-split.ts
+++ renderer/src/column-split.ts
@@ -20,3 +20,32 @@ export const columnSplit = <T>(
     }
     return columns.map(column => column.nodes);
 };
+
+if(import.meta.vitest) {
+    const { it, expect } = import.meta.vitest;
+    it("返り値の配列.length === columnsCount", () => {
+        expect(columnSplit([], 2, () => 0).length).toBe(2);
+    });
+    it("heightが小さい所を埋めるように入る", () => {
+        const nodes = [
+            { id: 1, height: 1 },
+            { id: 2, height: 3 },
+            { id: 3, height: 1 },
+            { id: 4, height: 2 },
+            { id: 5, height: 1 },
+            { id: 6, height: 1 },
+        ];
+        expect(columnSplit(nodes, 2, node => node.height)).toEqual([
+            [
+                { id: 1, height: 1 },
+                { id: 3, height: 1 },
+                { id: 4, height: 2 },
+                { id: 6, height: 1 },
+            ],
+            [
+                { id: 2, height: 3 },
+                { id: 5, height: 1 },
+            ],
+        ]);
+    });
+}
