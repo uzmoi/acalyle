2ebf4df17f7d90e7048f0c252be3803966bb81e8
rizzzse <rizzzse@gmail.com>
2022-10-14 02:16:56 +0900

    BookPage分離

--- renderer/src/pages/books/book.tsx
+++ renderer/src/pages/books/book.tsx
@@ -4,24 +4,12 @@ import { Router } from "~/shared/router/router";
 import { Book } from "~/widgets/book/Book";
 import type { RootRoutes } from "../routes";
 import { bookDeleteMutation } from "./__generated__/bookDeleteMutation.graphql";
-import { bookQuery, bookQuery$data } from "./__generated__/bookQuery.graphql";
+import { bookQuery } from "./__generated__/bookQuery.graphql";
 import { MemoPage } from "./memo";
 
-const route = Router.routes<
-    Router.GetRoute<RootRoutes, "books/:bookId">,
-    (book: NonNullable<bookQuery$data["book"]>) => JSX.Element
->({
-    /* eslint-disable react/display-name */
-    "": Router.page(params => book => <Book id={params.bookId} book={book} />),
-    ":memoId": Router.page(params => () => <MemoPage bookId={params.bookId} memoId={params.memoId} />),
-    settings: Router.page(() => () => <></>),
-    /* eslint-enable react/display-name */
-});
-
-export const BookPage: React.FC<{
-    path: readonly string[];
-    id: string;
-}> = ({ path, id }) => {
+const BookPage: React.FC<{
+    bookId: string;
+}> = ({ bookId }) => {
     const { book } = useLazyLoadQuery<bookQuery>(graphql`
         query bookQuery($id: ID!, $count: Int = 100, $cursor: String) {
             book(id: $id) {
@@ -30,8 +18,29 @@ export const BookPage: React.FC<{
                 ...BookMemosFragment
             }
         }
-    `, { id });
+    `, { id: bookId });
 
+    if(book == null) {
+        return <div>book not found (id: {bookId})</div>;
+    }
+
+    return (
+        <Book id={bookId} book={book} />
+    );
+};
+
+const route = Router.routes<Router.GetRoute<RootRoutes, "books/:bookId">, JSX.Element>({
+    /* eslint-disable react/display-name */
+    "": Router.page(params => <BookPage bookId={params.bookId} />),
+    ":memoId": Router.page(params => <MemoPage bookId={params.bookId} memoId={params.memoId} />),
+    settings: Router.page(() => <></>),
+    /* eslint-enable react/display-name */
+});
+
+export const BookChild: React.FC<{
+    path: readonly string[];
+    id: string;
+}> = ({ path, id }) => {
     const [commitDeleteBook, isInFlight] = useMutation<bookDeleteMutation>(graphql`
         mutation bookDeleteMutation($id: ID!) {
             deleteBook(id: $id)
@@ -39,10 +48,6 @@ export const BookPage: React.FC<{
     `);
 
     const navigate = useNavigate();
-
-    if(book == null) {
-        return <div>book not found (id: {id})</div>;
-    }
 
     const deleteBook = () => {
         commitDeleteBook({
@@ -55,7 +60,7 @@ export const BookPage: React.FC<{
 
     return (
         <div>
-            {route.get(path, { bookId: id })?.(book)}
+            {route.get(path, { bookId: id })}
             <button onClick={deleteBook} disabled={isInFlight}>delete book</button>
         </div>
     );
--- renderer/src/pages/books/routes.tsx
+++ renderer/src/pages/books/routes.tsx
@@ -1,11 +1,11 @@
 import { GetRoute, child, page, routes } from "~/shared/router/router";
 import type { RootRoutes } from "../routes";
-import { BookPage } from "./book";
+import { BookChild } from "./book";
 import { BookListPage } from "./books";
 import { NewBookPage } from "./new-book";
 
 export const booksRoute = routes<GetRoute<RootRoutes, "books">, JSX.Element>({
     "": page(() => <BookListPage />),
     new: page(() => <NewBookPage />),
-    ":bookId": child((path, params) => <BookPage path={path} id={params.bookId} />),
+    ":bookId": child((path, params) => <BookChild path={path} id={params.bookId} />),
 });
