df9518b0307bfedbc33fdeefe8ba270647b9440b
rizzzse <rizzzse@gmail.com>
2022-10-16 17:48:07 +0900

    refactor: rename to request

--- renderer/src/app/relay-env.ts
+++ renderer/src/app/relay-env.ts
@@ -1,9 +1,9 @@
 import { Environment, GraphQLResponse, Network, RecordSource, Store } from "relay-runtime";
 import { acalyle } from "./ipc";
 
-const network = Network.create(async (params, variables): Promise<GraphQLResponse> => {
-    if(params.text) {
-        const result = await acalyle.graphql(params.text, variables);
+const network = Network.create(async (request, variables): Promise<GraphQLResponse> => {
+    if(request.text) {
+        const result = await acalyle.graphql(request.text, variables);
         return {
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
             data: result.data || undefined!,
