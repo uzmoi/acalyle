3ba374c7b718a01ff792181108e71f1f40670d24
rizzzse <rizzzse@gmail.com>
2022-10-17 20:26:41 +0900

    add OmitPropNames

--- renderer/src/shared/control/text-input.tsx
+++ renderer/src/shared/control/text-input.tsx
@@ -1,9 +1,40 @@
 import { css, cx } from "@linaria/core";
 import { ControlPartBorderStyle, ControlPartResetStyle } from "./base";
 
+type OmitPropNames = (
+    // overrided
+    | "type"
+    | "autoComplete"
+    | "autoCapitalize"
+    | "autoCorrect"
+    | "spellCheck"
+    // numeric
+    | "min"
+    | "max"
+    | "step"
+    // type="checkbox", type="radio"
+    | "checked"
+    | "defaultChecked"
+    // type="file"
+    | "accept"
+    | "capture"
+    | "multiple"
+    // type="image"
+    | "src"
+    | "alt"
+    | "width"
+    | "height"
+    // type="image", type="submit"
+    | "formAction"
+    | "formEncType"
+    | "formMethod"
+    | "formNoValidate"
+    | "formTarget"
+);
+
 export const TextInput: React.FC<{
     onValueChange?: (value: string) => void;
-} & Omit<React.ComponentPropsWithoutRef<"input">, "type">> = ({
+} & Omit<React.ComponentPropsWithoutRef<"input">, OmitPropNames>> = ({
     onChange,
     onValueChange,
     className,
