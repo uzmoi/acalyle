eda0b5315743cf5134fbd13ee877e341000d7c5f
rizzzse <rizzzse@gmail.com>
2022-10-28 20:30:38 +0900

    remove "

--- renderer/src/entities/theme/index.ts
+++ renderer/src/entities/theme/index.ts
@@ -10,16 +10,16 @@ export const vars = themeNames("acalyle", {
 
 export const lightThemeStyle = vars.createTheme({
     color: {
-        "text": "#101018",
-        "bg": "#d0d0d0",
-        "bgsub": "#e0e0e0",
+        text: "#101018",
+        bg: "#d0d0d0",
+        bgsub: "#e0e0e0",
     },
 });
 
 export const darkThemeStyle = vars.createTheme({
     color: {
-        "text": "#e0e0e0",
-        "bg": "#101214",
-        "bgsub": "#181a1c",
+        text: "#e0e0e0",
+        bg: "#101214",
+        bgsub: "#181a1c",
     },
 });
