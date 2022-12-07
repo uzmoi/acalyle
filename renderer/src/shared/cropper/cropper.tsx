4137939de2d2bb49687b47a1bf2c80161a575f1e
rizzzse <rizzzse@gmail.com>
2022-12-07 17:52:21 +0900

    %でtranslateする

--- renderer/src/shared/cropper/cropper.tsx
+++ renderer/src/shared/cropper/cropper.tsx
@@ -1,5 +1,5 @@
 import { css, cx } from "@linaria/core";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useGrab } from "../global-event";
 
 interface Vec2 {
@@ -7,10 +7,14 @@ interface Vec2 {
     y: number;
 }
 
-const offsetPos = (e: MouseEvent, startTranslate: Vec2): Vec2 => {
+const offsetPos = (
+    e: MouseEvent,
+    startTranslate: Vec2,
+    el: HTMLElement,
+): Vec2 => {
     return {
-        x: startTranslate.x + e.clientX,
-        y: startTranslate.y + e.clientY,
+        x: (startTranslate.x + e.clientX) / el.clientWidth,
+        y: (startTranslate.y + e.clientY) / el.clientHeight,
     };
 };
 
@@ -20,31 +24,39 @@ export const Cropper: React.FC<{
     onChange?: (state: Vec2) => void;
     className?: string;
 }> = ({ src, state, onChange, className }) => {
+    const divEl = useRef<HTMLDivElement>(null);
     const [grabState, setGrabState] = useState<Vec2 | null>(null);
     const startGrab = useGrab<Vec2>((e, startTranslate) => {
-        setGrabState(offsetPos(e, startTranslate));
+        if (divEl.current != null) {
+            setGrabState(offsetPos(e, startTranslate, divEl.current));
+        }
     }, []);
     const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
         e.preventDefault();
         const startTranslate = {
-            x: state.x - e.clientX,
-            y: state.y - e.clientY,
+            x: state.x * e.currentTarget.clientWidth - e.clientX,
+            y: state.y * e.currentTarget.clientHeight - e.clientY,
         };
         startGrab(startTranslate, (e, startTranslate) => {
             setGrabState(null);
-            onChange?.(offsetPos(e, startTranslate));
+            if (divEl.current != null) {
+                onChange?.(offsetPos(e, startTranslate, divEl.current));
+            }
         });
     };
 
-    const transformState = grabState ?? state;
-    const transform = `translate(${transformState.x}px,${transformState.y}px)`;
+    const translate = grabState ?? state;
+    const transform = `translate(${translate.x * 100}%,${translate.y * 100}%)`;
 
     return (
         <div
+            ref={divEl}
             onMouseDown={handleMouseDown}
             className={cx(CropperStyle, className)}
         >
-            <img src={src} alt="" style={{ transform }} />
+            <div className={ImageStyle} style={{ transform }}>
+                <img src={src} alt="" />
+            </div>
         </div>
     );
 };
@@ -52,4 +64,9 @@ export const Cropper: React.FC<{
 const CropperStyle = css`
     overflow: hidden;
     background-color: #888;
+`;
+
+const ImageStyle = css`
+    width: 100%;
+    height: 100%;
 `;
