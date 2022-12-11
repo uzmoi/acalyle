ab3f7c33c15a16bac08df7b84fdb5738754cf292
rizzzse <rizzzse@gmail.com>
2022-12-12 00:55:21 +0900

    BookThumbnailFormにCropperを追加

--- renderer/src/features/book-form/book-thumbnail.tsx
+++ renderer/src/features/book-form/book-thumbnail.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { css } from "@linaria/core";
+import { useMemo, useState } from "react";
 import { graphql, useMutation } from "react-relay";
-import { Button, FileInput, Form } from "~/shared/control";
+import { Button, FileInput, Form, TextInput } from "~/shared/control";
+import { Cropper, cropImage } from "~/shared/cropper";
 import { bookThumbnailChangeMutation } from "./__generated__/bookThumbnailChangeMutation.graphql";
 
 export const BookThumbnailForm: React.FC<{
     bookId: string;
 }> = ({ bookId }) => {
     const [file, setFile] = useState<File | null>(null);
+    const fileUrl = useMemo(
+        () => (file ? URL.createObjectURL(file) : undefined),
+        [file],
+    );
 
     // prettier-ignore
     const [commitChangeThumbnail, isInFlight] = useMutation<bookThumbnailChangeMutation>(graphql`
@@ -16,11 +22,17 @@ export const BookThumbnailForm: React.FC<{
             }
         }
     `);
+
+    const [bgColor, setBgColor] = useState<string>("#888888");
+    const [cropState, setCropState] = useState({ x: 0, y: 0, scale: 1 });
+
     const handleSubmit = () => {
-        if (file == null) return;
-        commitChangeThumbnail({
-            variables: { id: bookId, thumbnail: null },
-            uploadables: { "variables.thumbnail": file },
+        if (fileUrl == null) return;
+        void cropImage(fileUrl, cropState, bgColor).then(blob => {
+            commitChangeThumbnail({
+                variables: { id: bookId, thumbnail: null },
+                uploadables: { "variables.thumbnail": blob },
+            });
         });
     };
 
@@ -33,9 +45,24 @@ export const BookThumbnailForm: React.FC<{
     return (
         <Form onSubmit={handleSubmit}>
             <FileInput onFileChange={handleFileChange} accept="image/*" />
+            <TextInput value={bgColor} onValueChange={setBgColor} />
+            <p>{Math.floor(cropState.scale * 100)}%</p>
+            <Cropper
+                src={fileUrl}
+                state={cropState}
+                onChange={setCropState}
+                className={CropperStyle}
+                bgColor={bgColor}
+            />
             <Button type="submit" disabled={file == null || isInFlight}>
                 Submit
             </Button>
         </Form>
     );
 };
+
+const CropperStyle = css`
+    width: 16em;
+    height: 16em;
+`;
+
