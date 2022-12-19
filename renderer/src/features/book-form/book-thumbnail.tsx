449c95f47806802d0d9a0c8022d9133ae73ac729
rizzzse <rizzzse@gmail.com>
2022-12-19 23:48:23 +0900

    UIの改善

--- renderer/src/features/book-form/book-thumbnail.tsx
+++ renderer/src/features/book-form/book-thumbnail.tsx
@@ -1,8 +1,13 @@
-import { css } from "@linaria/core";
-import { useMemo, useState } from "react";
+import { css, cx } from "@linaria/core";
+import { useId, useMemo, useState } from "react";
 import { graphql, useLazyLoadQuery, useMutation } from "react-relay";
+import { vars } from "~/entities/theme";
 import { Button, FileInput, Form, TextInput } from "~/shared/control";
-import { Cropper, cropImage } from "~/shared/cropper";
+import {
+    ControlPartOutlineStyle,
+    ControlPartResetStyle,
+} from "~/shared/control/base";
+import { Cropper, changeScale, cropImage } from "~/shared/cropper";
 import { bookThumbnailChangeMutation } from "./__generated__/bookThumbnailChangeMutation.graphql";
 import { bookThumbnailQuery } from "./__generated__/bookThumbnailQuery.graphql";
 
@@ -53,17 +58,95 @@ export const BookThumbnailForm: React.FC<{
         });
     };
 
-    const handleFileChange = (file: File | null) => {
-        if (file != null) {
-            setFile(file);
+    const handleFileChange = (newFile: File | null) => {
+        if (newFile != null) {
+            setFile(newFile);
         }
     };
 
+    const id = useId();
+
     return (
-        <Form onSubmit={handleSubmit}>
-            <FileInput onFileChange={handleFileChange} accept="image/*" />
-            <TextInput value={bgColor} onValueChange={setBgColor} />
-            <p>{Math.floor(cropState.scale * 100)}%</p>
+        <Form className={FormStyle} onSubmit={handleSubmit}>
+            <div className={InfoStyle}>
+                <h3
+                    className={css`
+                        margin-bottom: 0.6em;
+                    `}
+                >
+                    Thumbnail
+                </h3>
+                <div>
+                    <label
+                        className={cx(
+                            ControlPartResetStyle,
+                            ControlPartOutlineStyle,
+                            LabelStyle,
+                        )}
+                    >
+                        Select a image
+                        <FileInput
+                            onFileChange={handleFileChange}
+                            accept="image/*"
+                            className={HiddenStyle}
+                        />
+                    </label>
+                    <p
+                        className={css`
+                            font-size: 0.8em;
+                        `}
+                    >
+                        {file?.name}
+                    </p>
+                </div>
+                <dl>
+                    <dd>
+                        <label
+                            htmlFor={`${id}-bg-color`}
+                            className={LabelStyle}
+                        >
+                            background color
+                        </label>
+                    </dd>
+                    <dt>
+                        <TextInput
+                            id={`${id}-bg-color`}
+                            value={bgColor}
+                            onValueChange={setBgColor}
+                            aria-invalid={!CSS.supports("color", bgColor)}
+                        />
+                    </dt>
+                </dl>
+                <div>
+                    <p>{Math.floor(cropState.scale * 100)}%</p>
+                    <input
+                        type="range"
+                        min={10}
+                        max={1000}
+                        value={cropState.scale * 100}
+                        onChange={e => {
+                            const newScale =
+                                e.currentTarget.valueAsNumber / 100;
+                            setCropState(cropState =>
+                                changeScale(
+                                    cropState,
+                                    { x: 0, y: 0 },
+                                    newScale,
+                                ),
+                            );
+                        }}
+                    />
+                </div>
+                <div>
+                    <Button
+                        type="submit"
+                        className={SubmitButtonStyle}
+                        disabled={file == null || isInFlight}
+                    >
+                        Submit
+                    </Button>
+                </div>
+            </div>
             <Cropper
                 src={fileUrl}
                 state={cropState}
@@ -71,15 +154,40 @@ export const BookThumbnailForm: React.FC<{
                 className={CropperStyle}
                 bgColor={bgColor}
             />
-            <Button type="submit" disabled={file == null || isInFlight}>
-                Submit
-            </Button>
         </Form>
     );
 };
 
-const CropperStyle = css`
-    width: 16em;
-    height: 16em;
+const FormStyle = css`
+    display: flex;
 `;
 
+const InfoStyle = css`
+    position: relative;
+    flex: 1 1;
+    margin-right: 1em;
+`;
+
+const LabelStyle = css`
+    font-size: 0.875em;
+    font-weight: bold;
+`;
+
+const HiddenStyle = css`
+    width: 1px;
+    height: 1px;
+    visibility: hidden;
+`;
+
+const SubmitButtonStyle = css`
+    position: absolute;
+    right: 0;
+    bottom: 0;
+`;
+
+const CropperStyle = css`
+    flex: 0 0 12em;
+    width: 12em;
+    height: 12em;
+    border: 1px solid ${vars.color.text};
+`;
