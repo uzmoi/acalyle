4c8cdb65b19563c4498b9c3706538530a0beb9d0
rizzzse <rizzzse@gmail.com>
2022-12-28 20:18:47 +0900

    fix: ここ#いらんかった

--- renderer/src/pages/link.ts
+++ renderer/src/pages/link.ts
@@ -1,10 +1,4 @@
-import { type LinkBuilder, link as createLink } from "~/shared/router";
+import { link as createLink } from "~/shared/router";
 import type { RootRoutes } from "./routes";
 
-export const link: LinkBuilder<RootRoutes["path"]> = (
-    ...args: unknown[]
-): never => {
-    // @ts-expect-error めんどいので適当にお茶を濁してしまった
-    const x = createLink<RootRoutes>()(...args);
-    return ("#" + x) as never;
-};
+export const link = createLink<RootRoutes>();
