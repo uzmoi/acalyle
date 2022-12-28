ca675f41c5a611ede10cd8d46913ff6b925f40df
rizzzse <rizzzse@gmail.com>
2022-12-28 21:35:10 +0900

    リスト表示に対応

--- renderer/src/widgets/book/MemoList.tsx
+++ renderer/src/widgets/book/MemoList.tsx
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { graphql, usePaginationFragment } from "react-relay";
 import { MemoColumns } from "~/entities/memo/ui/memo-columns";
+import { MemoList as MemoList_ } from "~/entities/memo/ui/memo-list";
+import { Select } from "~/shared/control";
 import { MemoListFragment$key } from "./__generated__/MemoListFragment.graphql";
 import { MemoListPaginationQuery } from "./__generated__/MemoListPaginationQuery.graphql";
 
@@ -35,10 +37,28 @@ export const MemoList: React.FC<{
         refetch({ search }).dispose();
     }, [refetch, search]);
 
+    const [layoutStyle, setLayoutStyle] = useState<"columns" | "list">(
+        "columns",
+    );
+
+    const memos = useMemo(
+        () => data.memos.edges.map(edge => edge.node),
+        [data.memos.edges],
+    );
+
     return (
-        <MemoColumns
-            bookId={data.id}
-            memos={data.memos.edges.map(edge => edge.node)}
-        />
+        <div>
+            {/* @ts-expect-error FIXME onValueChangeがunionじゃない */}
+            <Select value={layoutStyle} onValueChange={setLayoutStyle}>
+                <Select.Option>columns</Select.Option>
+                <Select.Option>list</Select.Option>
+            </Select>
+            {
+                {
+                    columns: <MemoColumns bookId={data.id} memos={memos} />,
+                    list: <MemoList_ bookId={data.id} memos={memos} />,
+                }[layoutStyle]
+            }
+        </div>
     );
 };
