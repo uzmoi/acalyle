ec97eecd6a3103e84986cf979faaf9e2dfb50a59
rizzzse <rizzzse@gmail.com>
2023-02-05 13:12:44 +0900

    edit formを消す

--- renderer/src/widgets/book/Memo.tsx
+++ renderer/src/widgets/book/Memo.tsx
@@ -1,10 +1,7 @@
 import { css } from "@linaria/core";
-import { useState } from "react";
 import { graphql, useFragment } from "react-relay";
 import { MemoContents, MemoInfo } from "~/entities/memo";
 import { TagList } from "~/entities/tag";
-import { MemoContentsForm, MemoTagsForm } from "~/features/memo-form";
-import { Button } from "~/shared/control";
 import { MemoFragment$key } from "./__generated__/MemoFragment.graphql";
 
 export const Memo: React.FC<{
@@ -14,7 +11,6 @@ export const Memo: React.FC<{
     // prettier-ignore
     const memo = useFragment<MemoFragment$key>(graphql`
         fragment MemoFragment on Memo {
-            id
             contents
             tags
             createdAt
@@ -22,46 +18,15 @@ export const Memo: React.FC<{
         }
     `, fragmentRef);
 
-    const [isEditContents, setIsEditContents] = useState(false);
-    const [isEditTag, setIsEditTag] = useState(false);
-
     return (
         <div className={MemoStyle}>
-            {isEditContents ? (
-                <div>
-                    <MemoContentsForm
-                        memoId={memo.id}
-                        contents={memo.contents}
-                        onClose={() => setIsEditContents(false)}
-                    />
-                </div>
-            ) : (
-                <div>
-                    <MemoContents contents={memo.contents} />
-                    <Button onClick={() => setIsEditContents(true)}>
-                        edit
-                    </Button>
-                </div>
-            )}
+            <MemoContents contents={memo.contents} />
             <div className={MemoFooterStyle}>
                 <MemoInfo
                     createdAt={memo.createdAt}
                     updatedAt={memo.updatedAt}
                 />
-                {isEditTag ? (
-                    <MemoTagsForm
-                        memoId={memo.id}
-                        tags={memo.tags}
-                        onClose={() => setIsEditTag(false)}
-                    />
-                ) : (
-                    <>
-                        <TagList tags={memo.tags} bookId={bookId} />
-                        <Button onClick={() => setIsEditTag(true)}>
-                            edit tags
-                        </Button>
-                    </>
-                )}
+                <TagList tags={memo.tags} bookId={bookId} />
             </div>
         </div>
     );
