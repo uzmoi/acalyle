597b1b0a187e9cc290628f9a5f9da20f72163349
rizzzse <rizzzse@gmail.com>
2023-02-10 17:12:11 +0900

    style

--- renderer/src/pages/books/new-book.tsx
+++ renderer/src/pages/books/new-book.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { css } from "@linaria/core";
+import { useId, useState } from "react";
 import { graphql, useMutation } from "react-relay";
-import { BookTitleFormBlock } from "~/features/book-form";
 import { useNavigate } from "~/features/location";
 import { Button, Form, TextInput } from "~/shared/control";
 import { link } from "../link";
@@ -28,18 +28,78 @@ export const NewBookPage: React.FC = () => {
         });
     };
 
+    const htmlId = useId();
+    const titleId = `${htmlId}-title`;
+    const descriptionId = `${htmlId}-description`;
+
     return (
-        <Form onSubmit={onSubmit}>
-            <h1>Create a new book</h1>
-            <BookTitleFormBlock
-                title={title}
-                setTitle={setTitle}
-                disabled={isInFlight}
-            />
-            <TextInput value={description} onValueChange={setDescription} />
-            <Button type="submit" disabled={isInFlight}>
-                create book
-            </Button>
-        </Form>
+        <main
+            className={css`
+                padding: 2em;
+            `}
+        >
+            <Form onSubmit={onSubmit}>
+                <h1>Create a new book</h1>
+                <dl>
+                    <dt className={DTStyle}>
+                        <label htmlFor={titleId} className={LabelStyle}>
+                            Book title (required)
+                        </label>
+                    </dt>
+                    <dd>
+                        <TextInput
+                            id={titleId}
+                            className={css`
+                                width: 100%;
+                                min-width: 16em;
+                                max-width: 32em;
+                            `}
+                            value={title}
+                            onValueChange={setTitle}
+                            required
+                            maxLength={32}
+                            disabled={isInFlight}
+                        />
+                    </dd>
+                    <dt className={DTStyle}>
+                        <label htmlFor={descriptionId} className={LabelStyle}>
+                            Description
+                        </label>
+                    </dt>
+                    <dd>
+                        <TextInput
+                            id={descriptionId}
+                            className={css`
+                                width: 100%;
+                                min-width: 16em;
+                            `}
+                            value={description}
+                            onValueChange={setDescription}
+                            maxLength={500}
+                            disabled={isInFlight}
+                        />
+                    </dd>
+                </dl>
+                <Button
+                    type="submit"
+                    disabled={isInFlight}
+                    className={css`
+                        margin-top: 1em;
+                    `}
+                >
+                    Create book
+                </Button>
+            </Form>
+        </main>
     );
 };
+
+const DTStyle = css`
+    margin-top: 1em;
+    margin-bottom: 0.25em;
+`;
+
+const LabelStyle = css`
+    font-size: 0.9em;
+    font-weight: bold;
+`;
