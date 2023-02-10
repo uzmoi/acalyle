c10dee139fdf9d27196e71272b6b0bb8287f8fdc
rizzzse <rizzzse@gmail.com>
2023-02-10 20:56:00 +0900

    new-bookページにサムネアップロードを追加

--- renderer/src/pages/books/new-book.tsx
+++ renderer/src/pages/books/new-book.tsx
@@ -1,8 +1,10 @@
 import { css } from "@linaria/core";
-import { useId, useState } from "react";
+import { useId, useMemo, useState } from "react";
 import { graphql, useMutation } from "react-relay";
+import { BookThumbnailFormBlock } from "~/features/book-form/book-thumbnail";
 import { useNavigate } from "~/features/location";
 import { Button, Form, TextInput } from "~/shared/control";
+import { cropImage } from "~/shared/cropper";
 import { link } from "../link";
 import { newBookMutation } from "./__generated__/newBookMutation.graphql";
 
@@ -10,9 +12,25 @@ export const NewBookPage: React.FC = () => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
 
+    const [file, setFile] = useState<File | null>(null);
+    const fileUrl = useMemo(
+        () => (file ? URL.createObjectURL(file) : undefined),
+        [file],
+    );
+    const [bgColor, setBgColor] = useState<string>("#888888");
+    const [cropState, setCropState] = useState({ x: 0, y: 0, scale: 1 });
+
     const [commit, isInFlight] = useMutation<newBookMutation>(graphql`
-        mutation newBookMutation($title: String!, $description: String!) {
-            createBook(title: $title, description: $description) {
+        mutation newBookMutation(
+            $title: String!
+            $description: String!
+            $thumbnail: Upload
+        ) {
+            createBook(
+                title: $title
+                description: $description
+                thumbnail: $thumbnail
+            ) {
                 id
             }
         }
@@ -20,17 +38,32 @@ export const NewBookPage: React.FC = () => {
 
     const navigate = useNavigate();
     const onSubmit = () => {
-        commit({
-            variables: { title, description },
-            onCompleted({ createBook }) {
-                navigate(link("books/:bookId", { bookId: createBook.id }));
-            },
-        });
+        void (async () => {
+            const blob =
+                (fileUrl &&
+                    (await cropImage(
+                        fileUrl,
+                        { width: 512, height: 512 },
+                        cropState,
+                        bgColor,
+                    ))) ||
+                undefined;
+            commit({
+                variables: { title, description, thumbnail: null },
+                uploadables: blob && {
+                    "variables.thumbnail": blob,
+                },
+                onCompleted({ createBook }) {
+                    navigate(link("books/:bookId", { bookId: createBook.id }));
+                },
+            });
+        })();
     };
 
     const htmlId = useId();
     const titleId = `${htmlId}-title`;
     const descriptionId = `${htmlId}-description`;
+    const thumbnailId = `${htmlId}-thumbnail`;
 
     return (
         <main
@@ -77,6 +110,22 @@ export const NewBookPage: React.FC = () => {
                             onValueChange={setDescription}
                             maxLength={500}
                             disabled={isInFlight}
+                        />
+                    </dd>
+                    <dt className={DTStyle}>
+                        <label htmlFor={thumbnailId} className={LabelStyle}>
+                            Thumbnail
+                        </label>
+                    </dt>
+                    <dd>
+                        <BookThumbnailFormBlock
+                            file={file}
+                            fileUrl={fileUrl}
+                            setFile={setFile}
+                            cropState={cropState}
+                            setCropState={setCropState}
+                            bgColor={bgColor}
+                            setBgColor={setBgColor}
                         />
                     </dd>
                 </dl>
