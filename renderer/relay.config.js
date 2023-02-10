b3cfc59620334e9d5e8fbcee410eca3a78673222
rizzzse <rizzzse@gmail.com>
2023-02-10 21:25:18 +0900

    fix: configに絶対パスを使う

--- renderer/relay.config.js
+++ renderer/relay.config.js
@@ -1,9 +1,11 @@
-/* eslint-disable import/unambiguous */
+/* eslint-disable import/unambiguous, @typescript-eslint/no-var-requires */
+
+const path = require("path");
 
 // https://github.com/facebook/relay/blob/main/packages/relay-compiler/README.md
 module.exports = {
-    src: "./src",
-    schema: "../data/schema.graphql",
+    src: path.join(__dirname, "./src"),
+    schema: path.join(__dirname, "../data/schema.graphql"),
     customScalars: {
         Upload: "null",
         DateTime: "string",
