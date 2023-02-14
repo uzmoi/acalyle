589e1bd9740d37ab25c889a4556eb71ac3950658
rizzzse <rizzzse@gmail.com>
2023-02-14 19:52:15 +0900

    add Edit Button

--- renderer/src/widgets/book/Memo.tsx
+++ renderer/src/widgets/book/Memo.tsx
@@ -4,6 +4,7 @@ import { MemoInfo } from "~/entities/memo";
 import { TagList } from "~/entities/tag";
 import { vars } from "~/entities/theme";
 import { Markdown } from "~/features/markdown";
+import { Button } from "~/shared/control";
 import { MemoFragment$key } from "./__generated__/MemoFragment.graphql";
 
 export const Memo: React.FC<{
@@ -33,11 +34,27 @@ export const Memo: React.FC<{
             <Markdown contents={contents} />
             <footer
                 className={css`
+                    display: flex;
                     font-size: 0.8em;
                 `}
             >
-                <MemoInfo createdAt={createdAt} updatedAt={updatedAt} />
-                <TagList tags={tags} bookId={bookId} />
+                <div
+                    className={css`
+                        flex: 1;
+                    `}
+                >
+                    <MemoInfo createdAt={createdAt} updatedAt={updatedAt} />
+                    <TagList tags={tags} bookId={bookId} />
+                </div>
+                <div>
+                    <Button
+                        onClick={() => {
+                            // TODO
+                        }}
+                    >
+                        Edit
+                    </Button>
+                </div>
             </footer>
         </article>
     );
