16cabe42a8982423878b2ba7f9684d5d79ea2848
rizzzse <rizzzse@gmail.com>
2023-03-03 14:43:12 +0900

    type追加など

--- packages/core/src/tag.ts
+++ packages/core/src/tag.ts
@@ -1,21 +1,32 @@
+import { ValueOf, has } from "emnorst";
+
+export type TagHead = keyof typeof tagTypes;
+export type TagType = ValueOf<typeof tagTypes>;
+export type TagSymbol = `${TagHead}${string}`;
+
+const tagTypes = {
+    "#": "normal",
+    "@": "control",
+} as const;
+
 export class AcalyleMemoTag {
     static fromString(this: void, tagString: string): AcalyleMemoTag | null {
         const headChar = tagString.charAt(0);
-        const hasHead = "#@".includes(headChar);
-        const head = hasHead ? headChar : "#";
+        const hasHead = has(tagTypes, headChar);
+        const head = hasHead ? (headChar as TagHead) : "#";
         const i = head === "@" ? tagString.indexOf(":") : -1;
         const name = tagString.slice(+hasHead, i === -1 ? tagString.length : i);
         if (name === "") {
             return null;
         }
         const prop = tagString.slice(i + 1 || tagString.length) || null;
-        return new AcalyleMemoTag(head + name, prop);
+        return new AcalyleMemoTag(`${head}${name}`, prop);
     }
-    constructor(
-        readonly symbol: string,
-        readonly prop: string | null,
-    ) {}
+    constructor(readonly symbol: TagSymbol, readonly prop: string | null) {}
     toString(this: this): string {
         return this.symbol + (this.prop ? ":" + this.prop : "");
+    }
+    type(this: this): TagType {
+        return tagTypes[this.symbol[0] as TagHead];
     }
 }
