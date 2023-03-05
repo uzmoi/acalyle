b1ec683a00a67a171d371902b4366ce6be9c845a
rizzzse <rizzzse@gmail.com>
2023-03-05 23:16:35 +0900

    MenuをModalに置き換え

--- renderer/src/features/add-memo/AddMemoButton.tsx
+++ renderer/src/features/add-memo/AddMemoButton.tsx
@@ -1,6 +1,7 @@
-import { Button, ControlGroup } from "@acalyle/ui";
-import { Menu } from "@headlessui/react";
+import { Button, ControlGroup, Modal } from "@acalyle/ui";
 import { css } from "@linaria/core";
+import { useEffect, useState } from "react";
+import { WindowEvent } from "~/shared/global-event/window-event";
 import {
     AddTemplateMemoButtonList,
     type AddTemplateMemoButtonList$key,
@@ -14,34 +15,40 @@ export const AddMemoButton: React.FC<{
 }> = ({ bookId, onMemoAdded, data }) => {
     const [addMemo, isInFlight] = useAddMemo(bookId, onMemoAdded);
 
+    const [isOpenTemplatePopup, setIsOpenTemplatePopup] = useState(false);
+
+    useEffect(() => {
+        return WindowEvent.on("click", () => {
+            setIsOpenTemplatePopup(false);
+        });
+    }, []);
 
     return (
-        <Menu>
+        <div
+            className={css`
+                position: relative;
+            `}
+        >
             <ControlGroup>
                 <Button onClick={() => addMemo()} disabled={isInFlight}>
                     Add memo
                 </Button>
-                <Menu.Button as={Button}>▼</Menu.Button>
-            </ControlGroup>
-            <Menu.Items
-                className={css`
-                    position: relative;
-                `}
-            >
-                <div
-                    className={css`
-                        position: absolute;
-                        z-index: 100;
-                        white-space: nowrap;
-                        background-color: black;
-                    `}
+                <Button
+                    onClick={e => {
+                        e.stopPropagation();
+                        setIsOpenTemplatePopup(isOpen => !isOpen);
+                    }}
                 >
-                    <AddTemplateMemoButtonList
-                        data={data}
-                        onMemoAdd={addMemo}
-                    />
-                </div>
-            </Menu.Items>
-        </Menu>
+                    ▼
+                </Button>
+            </ControlGroup>
+            <Modal
+                open={isOpenTemplatePopup}
+                onClose={() => setIsOpenTemplatePopup(false)}
+                variant="popup"
+            >
+                <AddTemplateMemoButtonList data={data} onMemoAdd={addMemo} />
+            </Modal>
+        </div>
     );
 };
