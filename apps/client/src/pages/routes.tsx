075c89ecb26052c2adf63f62e9740dbe3b7dfcd1
rizzzse <rizzzse@gmail.com>
2023-03-21 12:31:37 +0900

    BookListPageのルートを/booksに変更

--- apps/client/src/pages/routes.tsx
+++ apps/client/src/pages/routes.tsx
@@ -4,13 +4,13 @@ import { BookPage, type BookPageRoute } from "./BookPage";
 import { NewBookPage } from "./NewBookPage";
 
 export type BookRoute = Router.Routes<{
-    "": Router.Page;
+    books: Router.Page;
     new: Router.Page;
     ":bookId": BookPageRoute;
 }>;
 
 export const BookRoute = Router.routes<BookRoute, JSX.Element>({
-    "": Router.page(() => <BookListPage />),
+    books: Router.page(() => <BookListPage />),
     new: Router.page(() => <NewBookPage />),
     ":bookId": Router.child(path => <BookPage path={path} />),
 });
