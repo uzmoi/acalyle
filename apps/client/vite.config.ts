207c3e338a5284a308abc9f4b0bf80f61fc7dd18
rizzzse <rizzzse@gmail.com>
2023-03-25 20:32:25 +0900

    fix: dtsもビルドしないとpathsの解決ができない

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,9 +1,17 @@
 import { macaronVitePlugin } from "@macaron-css/vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    plugins: [react(), macaronVitePlugin()],
+    plugins: [
+        react(),
+        macaronVitePlugin(),
+        dts({
+            exclude: "**/*.css.ts",
+            insertTypesEntry: true,
+        }),
+    ],
     resolve: {
         alias: { "~/": `${__dirname}/src/` },
     },
