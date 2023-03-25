8517fd17d6071edc378230b92425069fe523c9cf
rizzzse <rizzzse@gmail.com>
2023-03-25 16:30:53 +0900

    @acalyle/core単体でbuildする

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vitest/config";
+import { dependencies } from "./package.json";
 
 export default defineConfig({
     build: {
+        sourcemap: true,
         lib: {
             entry: "./src/index.ts",
             fileName: "index",
             formats: ["es"],
+        },
+        rollupOptions: {
+            external: Object.keys(dependencies),
         },
     },
 });
