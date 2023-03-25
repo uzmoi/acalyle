062254bba71bc715c938a8e6c63733a4e9bac42a
rizzzse <rizzzse@gmail.com>
2023-03-25 16:34:38 +0900

    fix: remove `export * as`

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -2,6 +2,3 @@ export * from "./pattern";
 export * from "./route";
 export { child, page, routes } from "./routes";
 export * from "./types";
-
-// eslint-disable-next-line import/no-self-import
-export * as Router from ".";
