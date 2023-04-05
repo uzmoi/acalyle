aa9fe2df66c523b6ff3fa650b61917b3006ad16a
rizzzse <rizzzse@gmail.com>
2023-04-05 22:12:21 +0900

    fix: キャッシュ消さない コンポーネントがsuspendするとlcが0になるので、キャッシュが消されて無限リロード編

--- apps/client/src/lib/query-store.ts
+++ apps/client/src/lib/query-store.ts
@@ -3,16 +3,13 @@ import { memoizeBuilder } from "~/lib/memoize-builder";
 import { createPromiseLoaderAtom } from "~/lib/promise-loader";
 
 export const createQueryStore = <T>(fetch: (id: string) => Promise<T>) => {
-    return memoizeBuilder((memoized, id: string) => {
+    return memoizeBuilder((_, id: string) => {
         const storeAtom = createPromiseLoaderAtom<T>();
 
         onMount(storeAtom, () => {
             if (storeAtom.get().status === "unpending") {
                 storeAtom.pending(fetch(id));
             }
-            return () => {
-                delete memoized.cache[id];
-            };
         });
 
         return storeAtom;
