a582a0bcd438dd1c999540b0254ddfb6c68c8af8
rizzzse <rizzzse@gmail.com>
2023-04-08 22:44:19 +0900

    add book handle store

--- apps/client/src/store/book.ts
+++ apps/client/src/store/book.ts
@@ -5,13 +5,16 @@ import type {
     GqlCreateBookMutation,
     GqlCreateBookMutationVariables,
 } from "~/__generated__/graphql";
+import { type PureAtom, derived, pure } from "~/lib/derived";
+import { memoizeBuilder } from "~/lib/memoize-builder";
+import type { PromiseLoaderW } from "~/lib/promise-loader";
 import { createQueryStore } from "~/lib/query-store";
 import type { Book } from "./book-connection";
 import { net } from "./net";
 
 const BookQuery = gql`
-    query Book($bookId: ID!) {
-        book(id: $bookId) {
+    query Book($bookId: ID, $handle: String) {
+        book(id: $bookId, handle: $handle) {
             id
             handle
             title
@@ -31,10 +34,49 @@ export const bookStore = createQueryStore(
             BookQuery,
             { bookId },
         );
-        return data.book
-            ? { ...data.book, handle: data.book.handle ?? null }
-            : null;
+        if (data.book == null) {
+            return null;
+        }
+        const book = {
+            ...data.book,
+            handle: data.book.handle ?? null,
+        };
+        if (book.handle != null) {
+            bookHandleStore(book.handle).resolve(book.id);
+        }
+        return book;
     },
+);
+
+export const bookHandleStore = createQueryStore(
+    async (handle: string): Promise<string | null> => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const { graphql } = net.get()!;
+        const { data } = await graphql<GqlBookQuery, GqlBookQueryVariables>(
+            BookQuery,
+            { handle },
+        );
+        if (data.book == null) {
+            return null;
+        }
+        const book = { ...data.book, handle };
+        bookStore(book.id).resolve(book);
+        return book.id;
+    },
+);
+
+export const handleBookStore = memoizeBuilder((_, handle: string) =>
+    derived((get): PureAtom<PromiseLoaderW<Book | null>> => {
+        const bookIdLoader = get(bookHandleStore(handle));
+        if (bookIdLoader.status !== "fulfilled" || bookIdLoader.value == null) {
+            return pure(
+                bookIdLoader as
+                    | { status: "fulfilled"; value: null }
+                    | Exclude<typeof bookIdLoader, { status: "fulfilled" }>,
+            );
+        }
+        return bookStore(bookIdLoader.value);
+    }),
 );
 
 const CreateBookMutation = gql`
