e6b5fc7e323e41f55c410dcd904c5b1c4c3670e4
rizzzse <rizzzse@gmail.com>
2023-04-13 22:50:26 +0900

    メモ用の検索バー追加

--- apps/client/src/lib/debounce.ts
+++ apps/client/src/lib/debounce.ts
@@ -1,17 +1,19 @@
 import { timeout } from "emnorst";
 
-export const debounce = (
-    f: () => void,
+export const debounce = <T extends readonly unknown[]>(
+    f: (...args: T) => void,
     ms = 800,
     options?: { signal?: AbortSignal },
 ) => {
+    let lastCallArgs: T;
     let lastCallTime = 0;
-    return () => {
+    return (...args: T) => {
+        lastCallArgs = args;
         lastCallTime = Date.now();
         const g = () => {
             const elapsedTime = Date.now() - lastCallTime;
             if (elapsedTime > ms) {
-                f();
+                f(...lastCallArgs);
             } else {
                 void timeout(ms - elapsedTime, options).then(g);
             }
