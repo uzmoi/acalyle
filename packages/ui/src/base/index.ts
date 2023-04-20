13ec878dca5b48e4cc1d5299514948152da8900b
rizzzse <rizzzse@gmail.com>
2023-04-21 00:19:00 +0900

    add Popover

--- packages/ui/src/base/index.ts
+++ packages/ui/src/base/index.ts
@@ -2,5 +2,6 @@ export * from "./cx";
 export * from "./intersection";
 export * from "./list";
 export * from "./modal";
+export * from "./popover";
 export * from "./spinner";
 export * from "./use-transition-status";
---
+++ packages/ui/src/base/popover.tsx
@@ -0,0 +1,133 @@
+import { style } from "@macaron-css/core";
+import { useStore } from "@nanostores/react";
+import { atom, onMount } from "nanostores";
+import { createContext, useCallback, useContext, useId } from "react";
+import { Button } from "../control/button";
+import { cx } from "./cx";
+import { useTransitionStatus } from "./use-transition-status";
+
+const PopoverStore = atom<string | null>(null);
+
+export const closePopover = () => {
+    PopoverStore.set(null);
+};
+
+onMount(PopoverStore, () => {
+    window.addEventListener("click", closePopover);
+    return () => {
+        window.removeEventListener("click", closePopover);
+    };
+});
+
+const PopoverIdContext = createContext<string | undefined>(undefined);
+
+export const Popover: React.FC<
+    {
+        className?: string;
+        children?: React.ReactNode;
+    } & React.ComponentPropsWithoutRef<"div">
+> & {
+    Button: typeof PopoverButton;
+    Content: typeof PopoverContent;
+} = ({ className, children, ...restProps }) => {
+    const id = useId();
+
+    return (
+        <div
+            {...restProps}
+            className={cx(
+                style({
+                    position: "relative",
+                }),
+                className,
+            )}
+        >
+            <PopoverIdContext.Provider value={id}>
+                {children}
+            </PopoverIdContext.Provider>
+        </div>
+    );
+};
+
+const PopoverButton: React.FC<
+    React.ComponentPropsWithoutRef<typeof Button>
+> = ({ onClick, children, ...restProps }) => {
+    const popoverId = useContext(PopoverIdContext);
+    const actualOnClick = useCallback(
+        (e: React.MouseEvent<HTMLButtonElement>) => {
+            e.stopPropagation();
+            onClick?.(e);
+            PopoverStore.set(
+                PopoverStore.get() === popoverId ? null : popoverId ?? null,
+            );
+        },
+        [onClick, popoverId],
+    );
+
+    return (
+        <Button {...restProps} onClick={actualOnClick}>
+            {children}
+        </Button>
+    );
+};
+
+Popover.Button = PopoverButton;
+if (import.meta.env.DEV) {
+    Popover.Button.displayName = "Popover.Button";
+}
+
+const transitionDuration = 200;
+
+const PopoverContent: React.FC<
+    {
+        closeOnClick?: boolean;
+    } & React.ComponentPropsWithoutRef<"div">
+> = ({ closeOnClick, onClick, className, children, ...restProps }) => {
+    const popoverId = useContext(PopoverIdContext);
+    const openedPopoverId = useStore(PopoverStore);
+    const isOpen = popoverId === openedPopoverId;
+    const status = useTransitionStatus({
+        show: isOpen,
+        transitionDuration,
+    });
+
+    const actualOnClick = closeOnClick
+        ? onClick
+        : (e: React.MouseEvent<HTMLDivElement>) => {
+              e.stopPropagation();
+              onClick?.(e);
+          };
+
+    return (
+        <div
+            {...restProps}
+            data-open={isOpen}
+            data-status={status}
+            className={cx(
+                style({
+                    position: "absolute",
+                    zIndex: 9999,
+                    transition: `opacity ${transitionDuration}ms`,
+                    selectors: {
+                        '&[data-open="true"]': {
+                            opacity: 1,
+                        },
+                        '&[data-open="false"]': {
+                            pointerEvents: "none",
+                            opacity: 0,
+                        },
+                    },
+                }),
+                className,
+            )}
+            onClick={actualOnClick}
+        >
+            {status === "exited" || children}
+        </div>
+    );
+};
+
+Popover.Content = PopoverContent;
+if (import.meta.env.DEV) {
+    Popover.Content.displayName = "Popover.Content";
+}
