9ab12768f7fecfe202d2e21aaf43b3291808896e
rizzzse <rizzzse@gmail.com>
2023-05-15 23:25:55 +0900

    impl BookSelectForm

--- apps/client/src/modal/select-book.tsx
+++ apps/client/src/modal/select-book.tsx
@@ -1,12 +1,17 @@
-import { Button, Form, openModal } from "@acalyle/ui";
+import { Button, ControlGroup, Form, List, openModal } from "@acalyle/ui";
 import { style } from "@macaron-css/core";
-import { useCallback } from "react";
+import { useStore } from "@nanostores/react";
+import { useCallback, useState } from "react";
+import { bookConnection } from "~/store/book-connection";
+import { BookOverview } from "~/ui/BookOverview";
+import { BookSearchBar } from "~/ui/BookSearchBar";
 
 export const selectBook = () => {
     return openModal<string | null>({
         default: null,
         render: close => (
             <div className={style({ padding: "1.25em" })}>
+                <BookSearchBar className={style({ marginBottom: "1em" })} />
                 <BookSelectForm onSubmit={close} />
             </div>
         ),
@@ -16,13 +21,54 @@ export const selectBook = () => {
 const BookSelectForm: React.FC<{
     onSubmit?: (bookId?: string | null) => void;
 }> = ({ onSubmit }) => {
+    const [bookId, setBookId] = useState<string | null>(null);
+    const { nodes } = useStore(bookConnection);
+
+    const handleSubmit = useCallback(() => {
+        onSubmit?.(bookId);
+    }, [onSubmit, bookId]);
+
     const cancel = useCallback(() => {
         onSubmit?.();
     }, [onSubmit]);
 
     return (
-        <Form>
-            <Button onClick={cancel}>Cancel</Button>
+        <Form onSubmit={handleSubmit}>
+            <List
+                className={style({
+                    maxHeight: "32em",
+                    overflowY: "auto",
+                    marginBottom: "1em",
+                })}
+            >
+                {nodes.map(book => (
+                    <List.Item
+                        key={book.id}
+                        data-selected={book.id === bookId}
+                        // HACK
+                        onClickCapture={e => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            setBookId(book.id);
+                        }}
+                        className={style({
+                            selectors: {
+                                '&[data-selected="true"]': {
+                                    outline: "1px solid red",
+                                },
+                            },
+                        })}
+                    >
+                        <BookOverview book={book} />
+                    </List.Item>
+                ))}
+            </List>
+            <ControlGroup>
+                <Button onClick={cancel}>Cancel</Button>
+                <Button type="submit" disabled={bookId == null}>
+                    Submit
+                </Button>
+            </ControlGroup>
         </Form>
     );
 };
