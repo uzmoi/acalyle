d918286b7e59c14eee3af2c298949cd6ddf781e6
rizzzse <rizzzse@gmail.com>
2023-05-21 19:23:32 +0900

    add MemoTag

--- apps/server/src/db/loader.rs
+++ apps/server/src/db/loader.rs
@@ -1,3 +1,7 @@
 pub struct SqliteLoader {
     pub pool: sqlx::SqlitePool,
 }
+
+pub struct SqliteTagLoader {
+    pub pool: sqlx::SqlitePool,
+}
--- apps/server/src/db/memo.rs
+++ apps/server/src/db/memo.rs
@@ -1,4 +1,4 @@
-use crate::db::loader::SqliteLoader;
+use super::loader::{SqliteLoader, SqliteTagLoader};
 use async_graphql::{
     async_trait::async_trait, dataloader::Loader, futures_util::TryStreamExt, Result,
 };
@@ -57,6 +57,64 @@ pub(crate) async fn insert_memos(
     });
     let query = query_builder.build();
 
+    query.execute(executor).await?;
+    Ok(())
+}
+
+#[derive(sqlx::FromRow, Clone)]
+pub(crate) struct MemoTag {
+    memo_id: String,
+    symbol: String,
+    prop: Option<String>,
+}
+
+#[async_trait]
+impl Loader<MemoId> for SqliteTagLoader {
+    type Value = Vec<String>;
+    type Error = Arc<sqlx::Error>;
+
+    async fn load(&self, keys: &[MemoId]) -> Result<HashMap<MemoId, Self::Value>, Self::Error> {
+        let mut query_builder =
+            sqlx::QueryBuilder::new("SELECT memoId, symbol, prop FROM Tag WHERE memoId IN");
+        query_builder.push_tuples(keys, |mut separated, key| {
+            separated.push_bind(key.0.clone());
+        });
+        let query = query_builder.build_query_as::<MemoTag>();
+
+        Ok(query
+            .fetch_all(&self.pool)
+            .await?
+            .iter()
+            .fold(HashMap::new(), |mut accum, tag| {
+                let MemoTag {
+                    memo_id,
+                    symbol,
+                    prop,
+                } = tag;
+                let tag_string = prop
+                    .clone()
+                    .map_or(symbol.clone(), |prop| format!("{symbol}:{prop}",));
+                accum
+                    .entry(MemoId(memo_id.clone()))
+                    .or_insert_with(Vec::new)
+                    .push(tag_string);
+                accum
+            }))
+    }
+}
+
+pub(crate) async fn insert_tags(
+    executor: impl SqliteExecutor<'_>,
+    tags: impl IntoIterator<Item = MemoTag>,
+) -> Result<()> {
+    let mut query_builder = sqlx::QueryBuilder::new("INSERT INTO Tag(memoId, symbol, prop) ");
+    query_builder.push_values(tags, |mut separated, tag| {
+        separated
+            .push_bind(tag.memo_id)
+            .push_bind(tag.symbol)
+            .push_bind(tag.prop);
+    });
+    let query = query_builder.build();
     query.execute(executor).await?;
     Ok(())
 }
