516b312f41e278e2d773ba8d8e5f4d8c88cdfca1
rizzzse <rizzzse@gmail.com>
2023-05-26 22:40:59 +0900

    開発時のdbをファイルにする

--- apps/server/src/bin/serve.rs
+++ apps/server/src/bin/serve.rs
@@ -1,10 +1,24 @@
 use acalyle_server::{db::init::create_tables, server::serve};
 use sqlx::{Result, SqlitePool};
+use std::{fs::OpenOptions, io, path::Path};
+
+fn touch(path: impl AsRef<Path>) -> io::Result<()> {
+    OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(path)
+        .map(|_| ())
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let pool = SqlitePool::connect("sqlite::memory:").await?;
-    create_tables(&pool).await?;
+    let db_file_path = "data/dev.db";
+    touch(db_file_path).unwrap();
+
+    let pool = SqlitePool::connect(&format!("sqlite:{db_file_path}")).await?;
+    if create_tables(&pool).await.is_ok() {
+        println!("Initialized {db_file_path}.");
+    };
 
     serve(&([127, 0, 0, 1], 4323).into(), pool).await;
 
