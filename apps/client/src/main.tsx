6ed9c6501bca1f8b7fcaabf404ad910d13e8f20c
rizzzse <rizzzse@gmail.com>
2023-07-16 23:51:21 +0900

    Locationを別ファイルに

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,31 +2,12 @@ import * as Router from "@acalyle/router";
 import { ModalContainer, vars } from "@acalyle/ui";
 import { globalStyle, style } from "@macaron-css/core";
 import { useStore } from "@nanostores/react";
-import { onMount } from "nanostores";
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { devAppTheme } from "./dev/theme";
 import { Location } from "./store/location";
 import { BookRoute } from ".";
-
-onMount(Location, () => {
-    const getLocation = () =>
-        location.pathname.split("/").filter(Boolean).join("/");
-    const unbind = Location.listen(path => {
-        if (path === getLocation()) return;
-        history.pushState(null, "", "/" + path);
-    });
-    Location.set(getLocation());
-    const popstate = (e: PopStateEvent) => {
-        e.preventDefault();
-        Location.set(getLocation());
-    };
-    window.addEventListener("popstate", popstate);
-    return () => {
-        unbind();
-        window.removeEventListener("popstate", popstate);
-    };
-});
+import "./dev/location";
 
 globalStyle("*, *::before, *::after", {
     boxSizing: "border-box",
