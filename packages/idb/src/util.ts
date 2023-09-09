175149f8ce6e87c0c47a59bb09f544fcce9877c3
rizzzse <rizzzse@gmail.com>
2023-09-09 22:10:28 +0900

    refactor

--- packages/idb/src/util.ts
+++ packages/idb/src/util.ts
@@ -1,17 +1,35 @@
+export const onAll = <T extends EventTarget>(
+    eventTarget: T,
+    handlers: {
+        [P in keyof T as P extends `on${infer K}` ? K : never]?: Extract<
+            T[P],
+            EventListener
+        >;
+    },
+) => {
+    const listeners = Object.entries<EventListenerOrEventListenerObject>(
+        handlers as Record<string, EventListenerOrEventListenerObject>,
+    );
+    for (const [type, listener] of listeners) {
+        eventTarget.addEventListener(type, listener);
+    }
+    return () => {
+        for (const [type, listener] of listeners) {
+            eventTarget.removeEventListener(type, listener);
+        }
+    };
+};
+
 export const requestToPromise = <T>(req: IDBRequest<T>): Promise<T> =>
     new Promise((resolve, reject) => {
-        const onSuccess = () => {
-            resolve(req.result);
-            off();
-        };
-        const onError = () => {
-            reject(req.error);
-            off();
-        };
-        req.addEventListener("success", onSuccess);
-        req.addEventListener("error", onError);
-        const off = () => {
-            req.removeEventListener("success", onSuccess);
-            req.removeEventListener("error", onError);
-        };
+        const offAll = onAll(req, {
+            success() {
+                resolve(req.result);
+                offAll();
+            },
+            error() {
+                reject(req.error);
+                offAll();
+            },
+        });
     });
