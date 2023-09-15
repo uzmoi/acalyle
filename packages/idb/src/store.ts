9b53a430d613a9d3f9f842584811bf988c09072d
rizzzse <rizzzse@gmail.com>
2023-09-15 17:36:09 +0900

    refactor

--- packages/idb/src/store.ts
+++ packages/idb/src/store.ts
@@ -10,45 +10,48 @@ export abstract class IdbStore<
     out Mode extends IDBTransactionMode,
 > implements IdbType<IDBIndex | IDBObjectStore, "keyPath">
 {
-    constructor(protected readonly store: S) {}
+    constructor(protected readonly _store: S) {}
     get name(): string {
-        return this.store.name;
+        return this._store.name;
+    }
+    get keyPath(): string | string[] {
+        return this._store.keyPath;
     }
     count(query?: IdbQuery): Promise<number> {
-        const req = this.store.count(query);
+        const req = this._store.count(query);
         return requestToPromise(req);
     }
     get(query: IdbQuery): Promise<T | undefined> {
-        const req = this.store.get(query);
+        const req = this._store.get(query);
         return requestToPromise(req as IDBRequest<T | undefined>);
     }
     getAll(query?: IdbQuery | null, count?: number): Promise<T[]> {
-        const req = this.store.getAll(query, count);
+        const req = this._store.getAll(query, count);
         return requestToPromise(req as IDBRequest<T[]>);
     }
     getAllKeys(
         query?: IdbQuery | null,
         count?: number,
     ): Promise<IDBValidKey[]> {
-        const req = this.store.getAllKeys(query, count);
+        const req = this._store.getAllKeys(query, count);
         return requestToPromise(req);
     }
     getKey(query: IdbQuery): Promise<IDBValidKey | undefined> {
-        const req = this.store.getKey(query);
+        const req = this._store.getKey(query);
         return requestToPromise(req);
     }
     openCursor(
         query?: IdbQuery | null,
         direction?: IDBCursorDirection,
     ): Promise<IdbCursor<T, Mode> | null> {
-        const req = this.store.openCursor(query, direction);
+        const req = this._store.openCursor(query, direction);
         return IdbCursor.from(req as IDBRequest<IDBCursor | null>);
     }
     openKeyCursor(
         query?: IdbQuery | null,
         direction?: IDBCursorDirection,
     ): Promise<IdbCursor<void, Mode> | null> {
-        const req = this.store.openKeyCursor(query, direction);
+        const req = this._store.openKeyCursor(query, direction);
         return IdbCursor.from(req);
     }
 }
@@ -62,13 +65,10 @@ export class IdbObjectStore<
     implements IdbType<IDBObjectStore, "index" | "transaction">
 {
     get autoIncrement(): boolean {
-        return this.store.autoIncrement;
-    }
-    get keyPath(): string | string[] {
-        return this.store.keyPath;
+        return this._store.autoIncrement;
     }
     get indexNames(): Indexes[] {
-        return Array.from(this.store.indexNames) as Indexes[];
+        return Array.from(this._store.indexNames) as Indexes[];
     }
     createIndex(
         this: IdbObjectStore<T, Indexes, "versionchange">,
@@ -76,23 +76,23 @@ export class IdbObjectStore<
         keyPath: string | Iterable<string>,
         options?: IDBIndexParameters,
     ): IdbIndex<T, Mode> {
-        return new IdbIndex(this.store.createIndex(name, keyPath, options));
+        return new IdbIndex(this._store.createIndex(name, keyPath, options));
     }
     deleteIndex(
         this: IdbObjectStore<T, Indexes, "versionchange">,
         name: string,
     ): void {
-        this.store.deleteIndex(name);
+        this._store.deleteIndex(name);
     }
     index(name: Indexes): IdbIndex<T, Mode> {
-        return new IdbIndex(this.store.index(name as string));
+        return new IdbIndex(this._store.index(name as string));
     }
     add(
         this: IdbObjectStore<T, Indexes, "readwrite">,
         value: T,
         key?: IDBValidKey,
     ): Promise<IDBValidKey> {
-        const req = this.store.add(value, key);
+        const req = this._store.add(value, key);
         return requestToPromise(req);
     }
     put(
@@ -100,18 +100,18 @@ export class IdbObjectStore<
         value: T,
         key?: IDBValidKey,
     ): Promise<IDBValidKey> {
-        const req = this.store.put(value, key);
+        const req = this._store.put(value, key);
         return requestToPromise(req);
     }
     delete(
         this: IdbObjectStore<T, Indexes, "readwrite">,
         query: IdbQuery,
     ): Promise<void> {
-        const req = this.store.delete(query);
+        const req = this._store.delete(query);
         return requestToPromise(req);
     }
     clear(this: IdbObjectStore<T, Indexes, "readwrite">): Promise<void> {
-        const req = this.store.clear();
+        const req = this._store.clear();
         return requestToPromise(req);
     }
 }
@@ -120,13 +120,10 @@ export class IdbIndex<T, out Mode extends IDBTransactionMode>
     extends IdbStore<IDBIndex, T, Mode>
     implements IdbType<IDBIndex, "objectStore">
 {
-    get keyPath(): string | string[] {
-        return this.store.keyPath;
-    }
     get unique(): boolean {
-        return this.store.unique;
+        return this._store.unique;
     }
     get multiEntry(): boolean {
-        return this.store.multiEntry;
+        return this._store.multiEntry;
     }
 }
