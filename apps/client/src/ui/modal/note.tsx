7403a2ec74999d42bbe63f4e6ab58b527133d0b7
rizzzse <rizzzse@gmail.com>
2023-09-23 21:55:54 +0900

    note modal

--- apps/client/src/ui/modal/note.tsx
+++ apps/client/src/ui/modal/note.tsx
@@ -1,4 +1,4 @@
-import { Button, openModal, vars } from "@acalyle/ui";
+import { Button, Modal, ModalContainer, vars } from "@acalyle/ui";
 import { style } from "@macaron-css/core";
 import { Suspense } from "react";
 import { BiExpandAlt, BiX } from "react-icons/bi";
@@ -7,65 +7,82 @@ import { link } from "~/pages/link";
 import { Link } from "../Link";
 import { Note } from "../note/Note";
 
+type NoteModalData = {
+    book: string;
+    memoId: Scalars["ID"];
+};
+
+const noteModal = Modal.create<NoteModalData, Scalars["ID"] | undefined>();
+
 export const openNoteInModal = (book: string, memoId: Scalars["ID"]) => {
-    return openModal({
-        default: null,
-        fullSize: true,
-        render: close => (
-            <section
+    return noteModal.open({ book, memoId });
+};
+
+export const renderNoteModal = () => (
+    <ModalContainer
+        modal={noteModal}
+        render={renderNoteModalContent}
+        size="max"
+    />
+);
+
+const close = () => {
+    void noteModal.close();
+};
+
+const renderNoteModalContent = ({ book, memoId }: NoteModalData) => (
+    <section
+        className={style({
+            display: "flex",
+            flexFlow: "column nowrap",
+            gap: "1em",
+            height: "100%",
+        })}
+    >
+        <header
+            className={style({
+                display: "flex",
+                flex: "0 0 2em",
+                borderRadius: "1em",
+                padding: "0.5em 0.75em",
+                backgroundColor: vars.color.bg.layout,
+                verticalAlign: "middle",
+            })}
+        >
+            <p className={style({ flex: "1 1 auto" })}>Note</p>
+            <div
                 className={style({
-                    display: "flex",
-                    flexFlow: "column nowrap",
-                    gap: "1em",
-                    height: "100%",
+                    flex: "0 0 auto",
+                    selectors: {
+                        "& + &": { paddingLeft: "0.5em" },
+                    },
                 })}
             >
-                <header
-                    className={style({
-                        display: "flex",
-                        flex: "0 0 2em",
-                        borderRadius: "1em",
-                        padding: "0.5em 0.75em",
-                        backgroundColor: vars.color.bg.layout,
-                        verticalAlign: "middle",
+                <Link
+                    to={link(":bookId/:memoId", {
+                        bookId: book,
+                        memoId,
                     })}
+                    onClick={close}
                 >
-                    <p className={style({ flex: "1 1 auto" })}>Note</p>
-                    <div
-                        className={style({
-                            flex: "0 0 auto",
-                            selectors: {
-                                "& + &": { paddingLeft: "0.5em" },
-                            },
-                        })}
-                    >
-                        <Link
-                            to={link(":bookId/:memoId", {
-                                bookId: book,
-                                memoId,
-                            })}
-                            onClick={() => close()}
-                        >
-                            <BiExpandAlt alt="view on page" />
-                        </Link>
-                        <Button variant="unstyled" onClick={() => close()}>
-                            <BiX />
-                        </Button>
-                    </div>
-                </header>
-                <div
-                    className={style({
-                        flex: "1 1 0",
-                        padding: "0.75em",
-                        overflow: "hidden auto",
-                        backgroundColor: vars.color.bg.layout,
-                    })}
-                >
-                    <Suspense>
-                        <Note book={book} noteId={memoId} />
-                    </Suspense>
-                </div>
-            </section>
-        ),
-    });
-};
+                    <BiExpandAlt alt="view on page" />
+                </Link>
+                <Button variant="unstyled" onClick={close}>
+                    <BiX />
+                </Button>
+            </div>
+        </header>
+        <div
+            className={style({
+                flex: "1 1 0",
+                padding: "0.75em",
+                overflow: "hidden auto",
+                backgroundColor: vars.color.bg.layout,
+            })}
+        >
+            <Suspense>
+                <Note book={book} noteId={memoId} />
+            </Suspense>
+        </div>
+    </section>
+);
--- apps/client/src/ui/modal/render-modals.tsx
+++ apps/client/src/ui/modal/render-modals.tsx
@@ -1,8 +1,10 @@
 import { renderConfirmModal } from "./confirm";
+import { renderNoteModal } from "./note";
 import { renderSelectBookModal } from "./select-book";
 
 export const renderModals = () => (
     <>
+        {renderNoteModal()}
         {renderConfirmModal()}
         {renderSelectBookModal()}
     </>
