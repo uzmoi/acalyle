620e42725d62192e185ff10c720e2b046929efea
rizzzse <rizzzse@gmail.com>
2023-09-24 15:19:23 +0900

    @relateの表示を削除

--- apps/client/src/ui/note/Note.tsx
+++ apps/client/src/ui/note/Note.tsx
@@ -1,13 +1,9 @@
-import { AcalyleMemoTag } from "@acalyle/core";
-import { style } from "@macaron-css/core";
 import { useStore } from "@nanostores/react";
 import type { Scalars } from "~/__generated__/graphql";
 import { usePromiseLoader } from "~/lib/promise-loader";
 import { memoStore } from "~/store/memo";
 import { NoteContents } from "./NoteContents";
 import { NoteHeader } from "./NoteHeader";
-import { NoteOverview } from "./NoteOverview";
-import { NoteOverviewWarpList } from "./NoteOverviewWarpList";
 
 const useNote = (noteId: Scalars["ID"]) => {
     const noteLoader = useStore(memoStore(noteId));
@@ -22,26 +18,10 @@ export const Note: React.FC<{
 
     if (note == null) return null;
 
-    const relateMemoId = note.tags
-        .map(AcalyleMemoTag.fromString)
-        .find(tag => tag?.symbol === "@relate")?.prop;
-
     return (
         <article data-note-id={noteId}>
-            {relateMemoId && (
-                <div className={style({ marginBottom: "0.5em" })}>
-                    <NoteOverview
-                        bookId={bookHandle as Scalars["ID"]}
-                        noteId={relateMemoId as Scalars["ID"]}
-                    />
-                </div>
-            )}
             <NoteHeader noteId={noteId} bookHandle={bookHandle} />
             <NoteContents noteId={noteId} />
-            <NoteOverviewWarpList
-                book={bookHandle}
-                query={`@relate:${noteId}`}
-            />
         </article>
     );
 };
