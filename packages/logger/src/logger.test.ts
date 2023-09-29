2d2f0aadf1cb3fe3d7c5474956984219c432d456
rizzzse <rizzzse@gmail.com>
2023-09-29 20:06:17 +0900

    add tests

--- packages/logger/src/logger.test.ts
+++ packages/logger/src/logger.test.ts
@@ -1,6 +1,6 @@
 import { noop } from "emnorst";
-import { expect, test, vi } from "vitest";
-import { BasicLogger } from ".";
+import { describe, expect, test, vi } from "vitest";
+import { BasicLogLevel, BasicLogger } from ".";
 
 test("transportがエラー吐いても継続する", () => {
     const logger = new BasicLogger("root");
@@ -13,4 +13,49 @@ test("transportがエラー吐いても継続する", () => {
     logger.info("log");
     expect(transport).toBeCalledTimes(2);
     expect(consoleError).toBeCalled();
+    consoleError.mockRestore();
+});
+
+describe("BasicLogger", () => {
+    test("log", () => {
+        const logger = new BasicLogger("root");
+        const transport = vi.fn();
+        logger.attachTransport({ transport });
+        logger.info("message");
+        expect(transport).lastCalledWith({
+            level: BasicLogLevel.info,
+            name: "root",
+            timeStamp: expect.any(Number) as number,
+            message: ["message"],
+            meta: undefined,
+        });
+    });
+    test("child", () => {
+        const logger = new BasicLogger("root");
+        const child = logger.child("child");
+        const transport = vi.fn();
+        logger.attachTransport({ transport });
+        child.info("message");
+        expect(transport).lastCalledWith({
+            level: BasicLogLevel.info,
+            name: "root.child",
+            timeStamp: expect.any(Number) as number,
+            message: ["message"],
+            meta: undefined,
+        });
+    });
+    test("mapChild", () => {
+        const logger = new BasicLogger<string>("root");
+        const child = logger.mapChild<unknown>("child", JSON.stringify);
+        const transport = vi.fn();
+        logger.attachTransport({ transport });
+        child.info("message");
+        expect(transport).lastCalledWith({
+            level: BasicLogLevel.info,
+            name: "root.child",
+            timeStamp: expect.any(Number) as number,
+            message: ['"message"'],
+            meta: undefined,
+        });
+    });
 });
