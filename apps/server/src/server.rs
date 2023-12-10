b5ad3a99c56f37828b4b5c9401bfeec21e68d91d
uzmoi <rizzzse@gmail.com>
2023-12-10 19:39:36 +0900

    remove extra &

--- apps/server/src/server.rs
+++ apps/server/src/server.rs
@@ -23,7 +23,7 @@ struct Endpoint {
 // cspell:word graphiql
 async fn graphiql_handler(req: Query<Endpoint>) -> impl response::IntoResponse {
     let endpoint = req.0.endpoint.as_deref().unwrap_or("/");
-    response::Html(GraphiQLSource::build().endpoint(&endpoint).finish())
+    response::Html(GraphiQLSource::build().endpoint(endpoint).finish())
 }
 
 async fn resource_handler(
