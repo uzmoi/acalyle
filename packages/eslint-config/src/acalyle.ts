c3ad78cab08218f997f020beefe1fed010c49214
uzmoi <rizzzse@gmail.com>
2024-02-25 19:43:26 +0900

    use eslint-plugin-pure-module

--- packages/eslint-config/src/acalyle.ts
+++ packages/eslint-config/src/acalyle.ts
@@ -1,4 +1,5 @@
 import { ESLint, Linter } from "eslint";
+import pureModule from "eslint-plugin-pure-module";
 import { rules } from "./rules";
 import { typescriptFiles } from "./typescript";
 import { WARN, warn } from "./util";
@@ -8,7 +9,10 @@ export const acalylePlugin: ESLint.Plugin = { rules };
 export const acalyleConfig: Linter.FlatConfig[] = [
     {
         files: [typescriptFiles],
-        plugins: { acalyle: acalylePlugin },
+        plugins: {
+            acalyle: acalylePlugin,
+            "pure-module": pureModule as unknown as ESLint.Plugin,
+        },
         rules: {
             "acalyle/prefer-string-literal": WARN,
         },
@@ -22,7 +26,7 @@ export const acalyleConfig: Linter.FlatConfig[] = [
             "**/__{test,spec}?(s)__/**",
         ],
         rules: {
-            "acalyle/no-module-side-effect": warn({
+            "pure-module/pure-module": warn({
                 pureFunctions: [
                     // vite
                     "import.meta.hot.*",
