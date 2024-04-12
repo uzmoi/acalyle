fa01511b313639e3fe765ca79db6240f3b838702
uzmoi <rizzzse@gmail.com>
2024-04-12 20:50:15 +0900

    add Menu component

--- packages/ui/src/control/index.ts
+++ packages/ui/src/control/index.ts
@@ -3,6 +3,7 @@ export * from "./button";
 export * from "./file-input";
 export * from "./form";
 export * from "./group";
+export * from "./menu";
 export * from "./select";
 export * from "./text-input";
 export * from "./textarea";
