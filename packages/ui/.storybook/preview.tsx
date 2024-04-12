57d7f71951f607f8676d07819d21e92615abe2cf
uzmoi <rizzzse@gmail.com>
2024-04-13 01:57:32 +0900

    fix

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -1,33 +1,7 @@
 import type { Preview } from "@storybook/react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from "react";
-import { createTheme, vars } from "../src";
-
-const theme = createTheme<typeof vars>("acalyle", {
-    color: {
-        bg: {
-            app: "#ddd",
-            layout: "#ccc",
-            block: "#bbb",
-            inline: "#aaa",
-        },
-        fg: {
-            __: "#222",
-            mute: "#777",
-        },
-        accent: "purple",
-        danger: "red",
-        warning: "yellow",
-        success: "green",
-    },
-    radius: {
-        block: "0.25m",
-        control: "0.5em",
-        layout: "1em",
-    },
-    font: { mono: "monospace", sans: "sans-serif" },
-    zIndex: { contextMenu: 0, max: 0, modal: 0, popover: 0, toast: 0 },
-});
+import { devTheme } from "../src/theme/test-theme";
 
 const preview: Preview = {
     parameters: {
@@ -39,7 +13,8 @@ const preview: Preview = {
             },
         },
     },
-    decorators: [story => <div style={theme}>{story()}</div>],
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    decorators: [story => <div style={devTheme}>{story()}</div>],
 };
 
 export default preview;
