71b88956c54312f5fcc07e40787fabca6fc3341f
uzmoi <rizzzse@gmail.com>
2024-04-24 17:15:38 +0900

    move pure-module rule

--- packages/eslint-config/src/acalyle.ts
+++ packages/eslint-config/src/acalyle.ts
@@ -1,36 +1,15 @@
 import { ESLint, Linter } from "eslint";
-import pureModule from "eslint-plugin-pure-module";
 import { rules } from "./rules";
 import { WARN, tsExts } from "./util";
 
 export const acalylePlugin: ESLint.Plugin = { rules };
 
-export const acalyleConfig: Linter.FlatConfig[] = [
-    {
-        files: [`**/*.${tsExts}`],
-        plugins: {
-            acalyle: acalylePlugin,
-            "pure-module": pureModule as unknown as ESLint.Plugin,
-        },
-        rules: {
-            "acalyle/prefer-string-literal": WARN,
-        },
+export const acalyleConfig: Linter.FlatConfig = {
+    files: [`**/*.${tsExts}`],
+    plugins: {
+        acalyle: acalylePlugin,
     },
-    {
-        files: [`**/src/**/*.${tsExts}`],
-        ignores: ["**/*.{test,test-d,stories}.*"],
-        rules: {
-            "pure-module/pure-module": warn({
-                pureFunctions: [
-                    // vite
-                    "import.meta.hot.*",
-                    // zero-runtime css-in-js
-                    "style",
-                    "styled",
-                    "styled.*",
-                    "css",
-                ],
-            }),
-        },
+    rules: {
+        "acalyle/prefer-string-literal": WARN,
     },
-];
+};
--- packages/eslint-config/src/recommended.ts
+++ packages/eslint-config/src/recommended.ts
@@ -1,61 +1,84 @@
 import eslint from "@eslint/js";
 import type { ESLint, Linter } from "eslint";
 import perfectionist from "eslint-plugin-perfectionist";
+import pureModule from "eslint-plugin-pure-module";
 import unicornPlugin from "eslint-plugin-unicorn";
-import { ERROR, OFF, WARN, replaceWarn, unPartial, warn } from "./util";
+import { ERROR, OFF, WARN, replaceWarn, tsExts, unPartial, warn } from "./util";
 
-export const recommended: Linter.FlatConfig = {
-    plugins: {
-        unicorn: unicornPlugin,
-        perfectionist,
-    },
-    rules: {
-        ...eslint.configs.recommended.rules,
-        ...replaceWarn(
-            unPartial(
-                (unicornPlugin.configs?.recommended as ESLint.ConfigData)
-                    .rules!,
+export const recommended: Linter.FlatConfig[] = [
+    {
+        plugins: {
+            unicorn: unicornPlugin,
+            perfectionist,
+        },
+        rules: {
+            ...eslint.configs.recommended.rules,
+            ...replaceWarn(
+                unPartial(
+                    (unicornPlugin.configs?.recommended as ESLint.ConfigData)
+                        .rules!,
+                ),
             ),
-        ),
-        "unicorn/filename-case": warn({
-            cases: { kebabCase: true, pascalCase: true },
-        }),
-        "unicorn/import-style": OFF,
-        "unicorn/no-array-callback-reference": OFF,
-        "unicorn/no-array-reduce": OFF,
-        "unicorn/no-for-loop": OFF,
-        "unicorn/no-nested-ternary": OFF,
-        "unicorn/no-null": OFF,
-        "unicorn/no-this-assignment": OFF,
-        "unicorn/no-unsafe-regex": ERROR,
-        "unicorn/no-unused-properties": WARN,
-        "unicorn/prefer-at": OFF,
-        "unicorn/prefer-code-point": OFF,
-        "unicorn/prefer-module": OFF,
-        "unicorn/prefer-number-properties": warn({ checkInfinity: false }),
-        "unicorn/prefer-query-selector": OFF,
-        "unicorn/prefer-string-replace-all": OFF,
-        "unicorn/prevent-abbreviations": OFF,
-        "unicorn/template-indent": warn({
-            indent: 4,
-            tags: [],
-            selectors: ["TaggedTemplateExpression"],
-        }),
-        ...replaceWarn(
-            unPartial(
-                (
-                    perfectionist.configs?.[
-                        "recommended-natural"
-                    ] as ESLint.ConfigData
-                ).rules!,
+            "unicorn/filename-case": warn({
+                cases: { kebabCase: true, pascalCase: true },
+            }),
+            "unicorn/import-style": OFF,
+            "unicorn/no-array-callback-reference": OFF,
+            "unicorn/no-array-reduce": OFF,
+            "unicorn/no-for-loop": OFF,
+            "unicorn/no-nested-ternary": OFF,
+            "unicorn/no-null": OFF,
+            "unicorn/no-this-assignment": OFF,
+            "unicorn/no-unsafe-regex": ERROR,
+            "unicorn/no-unused-properties": WARN,
+            "unicorn/prefer-at": OFF,
+            "unicorn/prefer-code-point": OFF,
+            "unicorn/prefer-module": OFF,
+            "unicorn/prefer-number-properties": warn({ checkInfinity: false }),
+            "unicorn/prefer-query-selector": OFF,
+            "unicorn/prefer-string-replace-all": OFF,
+            "unicorn/prevent-abbreviations": OFF,
+            "unicorn/template-indent": warn({
+                indent: 4,
+                tags: [],
+                selectors: ["TaggedTemplateExpression"],
+            }),
+            ...replaceWarn(
+                unPartial(
+                    (
+                        perfectionist.configs?.[
+                            "recommended-natural"
+                        ] as ESLint.ConfigData
+                    ).rules!,
+                ),
             ),
-        ),
-        "perfectionist/sort-imports": OFF,
-        "perfectionist/sort-interfaces": OFF,
-        "perfectionist/sort-classes": OFF,
-        "perfectionist/sort-jsx-props": OFF,
-        "perfectionist/sort-object-types": OFF,
-        "perfectionist/sort-objects": OFF,
-        "perfectionist/sort-union-types": OFF,
+            "perfectionist/sort-imports": OFF,
+            "perfectionist/sort-interfaces": OFF,
+            "perfectionist/sort-classes": OFF,
+            "perfectionist/sort-jsx-props": OFF,
+            "perfectionist/sort-object-types": OFF,
+            "perfectionist/sort-objects": OFF,
+            "perfectionist/sort-union-types": OFF,
+        },
     },
-};
+    {
+        files: [`**/src/**/*.${tsExts}`],
+        ignores: ["**/*.{test,test-d,stories}.*"],
+        plugins: {
+            "pure-module": pureModule as unknown as ESLint.Plugin,
+        },
+        rules: {
+            "pure-module/pure-module": warn({
+                pureFunctions: [
+                    // vite
+                    "import.meta.hot.*",
+                    // zero-runtime css-in-js
+                    "style",
+                    "styled",
+                    "styled.*",
+                    "css",
+                ],
+            }),
+        },
+    },
+];
