81d6e877091b17678310fd208a429e1889c2933d
uzmoi <rizzzse@gmail.com>
2025-01-18 22:34:28 +0900

    bookのレイアウト

--- apps/client/src/routes/books/$book-id.tsx
+++ apps/client/src/routes/books/$book-id.tsx
@@ -1,10 +1,29 @@
-import { Outlet, createFileRoute, useParams } from "@tanstack/react-router";
+import {
+  Link,
+  Outlet,
+  createFileRoute,
+  useParams,
+} from "@tanstack/react-router";
+import { useBook } from "~/store/hook";
 
 const LayoutComponent: React.FC = () => {
-  const { "book-id": _bookId } = useParams({ from: Route.fullPath });
+  const { "book-id": bookRef } = useParams({ from: Route.fullPath });
+  const book = useBook(bookRef);
+
+  if (book == null) {
+    return (
+      <div className=":uno: px-8 py-4 text-center">
+        <p>Book {bookRef} not found</p>
+        <Link to="/books">Return /books</Link>
+      </div>
+    );
+  }
 
   return (
-    <div>
+    <div className=":uno: px-8 py-4">
+      <h1>{book.title}</h1>
+      <p className=":uno: text-xs">{book.description}</p>
+      <hr className=":uno: my-2 border-gray-7 border-none border-t-solid" />
       <Outlet />
     </div>
   );
