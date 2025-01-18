a897877aa86ce2b4783609dc5b5c160137ab3504
uzmoi <rizzzse@gmail.com>
2025-01-18 22:37:17 +0900

    book page

--- apps/client/src/routes/books/$book-id/index.tsx
+++ apps/client/src/routes/books/$book-id/index.tsx
@@ -1,9 +1,32 @@
+import { TextInput } from "@acalyle/ui";
 import { createFileRoute, useParams } from "@tanstack/react-router";
+import { useDeferredValue, useState } from "react";
+import type { BookRef } from "~/book/store";
+import { NoteOverviewWarpList } from "~/note/ui/note-overview-warplist";
+import { CreateMemoButton } from "~/ui/CreateMemoButton";
 
 const RouteComponent: React.FC = () => {
-  const { "book-id": _bookId } = useParams({ from: "/books/$book-id/" });
+  const { "book-id": bookRef } = useParams({ from: Route.fullPath });
 
-  return <div></div>;
+  const [query, setQuery] = useState("");
+  const deferredQuery = useDeferredValue(query);
+
+  return (
+    <div>
+      <div className=":uno: mb-4 flex gap-4">
+        <TextInput
+          type="search"
+          className=":uno: flex-1"
+          onValueChange={setQuery}
+        />
+        <CreateMemoButton bookHandle={bookRef} />
+      </div>
+      <NoteOverviewWarpList
+        bookRef={bookRef as BookRef}
+        query={`-@relate:* ${deferredQuery}`}
+      />
+    </div>
+  );
 };
 
 export const Route = /* #__PURE__ */ createFileRoute("/books/$book-id/")({
