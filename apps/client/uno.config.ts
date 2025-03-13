00316f8c11a8465d3b94e383d634e4fae8537eba
uzmoi <rizzzse@gmail.com>
2025-03-13 20:16:13 +0900

    chore: VSCodeがunocssのクラスを検出できるようにする

--- apps/client/uno.config.ts
+++ apps/client/uno.config.ts
@@ -8,7 +8,17 @@ import {
 const extractor: Extractor = {
     name: "@acalyle/extractor-compile-class",
     extract({ code }) {
-        return code.match(/uno-[\w-]+/g) ?? [];
+        // 開発時にエディタがクラスを検出するため
+        const rawUnoCompileClasses =
+            code
+                .match(/:uno-?[^\s:]*:(?:\s+[^\s"';`{}]+)+/g)
+                ?.flatMap(x => x.split(" "))
+                .filter(x => x !== "" && !x.startsWith(":uno")) ?? [];
+
+        // cssの抽出時はtransformerCompileClassが先に適用される
+        const compiledClasses = code.match(/uno-[\w-]+/g) ?? [];
+
+        return [...rawUnoCompileClasses, ...compiledClasses];
     },
 };
 
