1c02cfe396f4437313920cd82f7e2452e0537388
uzmoi <rizzzse@gmail.com>
2025-03-18 17:16:49 +0900

    update dev Provider

--- apps/client/src/dev/provider.tsx
+++ apps/client/src/dev/provider.tsx
@@ -1,21 +1,27 @@
-import { style } from "@acalyle/css";
+import { cx, style } from "@acalyle/css";
 import { vars } from "@acalyle/ui";
+import { defaultTheme } from "~/theme/default";
+import { theme } from "../theme";
 import { devAppTheme } from "./theme";
 
 export const Provider: React.FC<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }> = ({ children }) => {
-    return (
-        <div
-            style={devAppTheme}
-            className={style({
-                minHeight: "100%",
-                fontFamily: vars.font.sans,
-                color: vars.color.fg.__,
-                backgroundColor: vars.color.bg.app,
-            })}
-        >
-            {children}
-        </div>
-    );
+  return (
+    <div
+      // TODO: テーマの参照の移行が終わったら消す
+      style={devAppTheme}
+      className={cx(
+        defaultTheme,
+        style({
+          minHeight: "100%",
+          fontFamily: vars.font.sans,
+          color: theme("app-text"),
+          backgroundColor: theme("app-bg"),
+        }),
+      )}
+    >
+      {children}
+    </div>
+  );
 };
