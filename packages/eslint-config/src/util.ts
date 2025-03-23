688baa2c26c53dda45b8d7398068a933981c65dd
uzmoi <rizzzse@gmail.com>
2025-03-23 23:04:10 +0900

    remove unused utils

--- packages/eslint-config/src/util.ts
+++ packages/eslint-config/src/util.ts
@@ -1,7 +1,5 @@
 import type { Linter } from "eslint";
 
-export const tsExts = "{ts,mts,cts,tsx}";
-
 export const always = "always";
 export const never = "never";
 
@@ -14,16 +12,6 @@ export const ERROR = "error";
 export const error = <T extends unknown[]>(
   ...options: T
 ): Linter.RuleEntry<T> => [ERROR, ...options];
-
-export const memoize = <T, U>(f: (arg: T) => U) => {
-  const cache = new Map<T, U>();
-  return (arg: T): U => {
-    if (!cache.has(arg)) {
-      cache.set(arg, f(arg));
-    }
-    return cache.get(arg)!;
-  };
-};
 
 export const unPartial = <T>(object: Partial<T>): Required<T> =>
   object as Required<T>;
@@ -51,18 +39,6 @@ export const mapEntries = <T extends object, U extends object>(
   ) as U;
 };
 
-export const replacePluginName = (
-  rules: Linter.RulesRecord,
-  plugins: Record<string, string>,
-): Linter.RulesRecord => {
-  const regex = new RegExp(`^${Object.keys(plugins).join("|")}\\/`);
-  const resolve = (plugin: string) => `${plugins[plugin] ?? ""}/`;
-  return mapEntries(rules, (ruleName, ruleEntry) => [
-    ruleName.replace(regex, resolve),
-    ruleEntry,
-  ]);
-};
-
 export const replaceWarn = (
   rules: Partial<Linter.RulesRecord>,
 ): Linter.RulesRecord => {
@@ -73,33 +49,4 @@ export const replaceWarn = (
     : entry === ERROR ? WARN
     : (entry ?? OFF),
   ]);
-};
-
-export const asArray = <T>(value: T | readonly T[] | null | undefined): T[] =>
-  Array.isArray(value) ? [...value]
-  : value == null ? []
-  : [value as T];
-
-export const extendsFlatRules = (
-  plugin: { configs?: Record<string, unknown> },
-  configNames: readonly string[],
-  mapRules: (
-    rules: Linter.RulesRecord,
-    configName: string,
-  ) => Linter.RulesRecord,
-) => {
-  const result: Linter.RulesRecord = {};
-  for (const configName of configNames) {
-    const configs = asArray(
-      plugin.configs?.[configName],
-    ) as Linter.FlatConfig[];
-    const lastConfig = configs.pop();
-    Object.assign(
-      result,
-      ...configs.map(config => config.rules),
-      // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-      mapRules(lastConfig?.rules!, configName),
-    );
-  }
-  return result;
 };
