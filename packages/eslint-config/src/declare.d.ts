7fa5d790d5e0680cf88f0135976a3d2ae076bfd1
uzmoi <rizzzse@gmail.com>
2025-03-23 22:22:42 +0900

    update eslint-plugin-testing-library

--- packages/eslint-config/src/declare.d.ts
+++ packages/eslint-config/src/declare.d.ts
@@ -29,10 +29,3 @@ declare module "eslint-plugin-import" {
   };
   export default exports;
 }
-
-declare module "eslint-plugin-testing-library" {
-  import type { ESLint } from "eslint";
-
-  const exports: ESLint.Plugin;
-  export default exports;
-}
