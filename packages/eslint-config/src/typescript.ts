7105b460237f52c0204c03285e6b6e606ce41d6e
uzmoi <rizzzse@gmail.com>
2025-03-25 00:14:39 +0900

    improve typescript lint config

--- packages/eslint-config/src/typescript.ts
+++ packages/eslint-config/src/typescript.ts
@@ -1,6 +1,6 @@
 import type { Linter } from "eslint";
 import tsEslint from "typescript-eslint";
-import { OFF, WARN, replaceWarn, warn } from "./util";
+import { OFF, replaceWarn, warn } from "./util";
 
 const stylisticTypeChecked = (
   tsEslint.configs.stylisticTypeChecked as Linter.Config[]
@@ -14,19 +14,40 @@ export const typescript: Linter.Config[] = [
   },
   {
     rules: {
+      // (recommendedTypeChecked | stylisticTypeChecked) & oxlint
+      "@typescript-eslint/adjacent-overload-signatures": OFF,
+      "@typescript-eslint/array-type": OFF,
+      "@typescript-eslint/ban-ts-comment": OFF,
+      "@typescript-eslint/consistent-generic-constructors": OFF,
+      "@typescript-eslint/consistent-indexed-object-style": OFF,
       "@typescript-eslint/consistent-type-definitions": OFF,
-      "@typescript-eslint/no-restricted-types": WARN,
-      "@typescript-eslint/no-unsafe-function-type": WARN,
-      "@typescript-eslint/no-wrapper-object-types": WARN,
-      "@typescript-eslint/no-namespace": warn({
-        allowDeclarations: true,
-        allowDefinitionFiles: true,
-      }),
-      "@typescript-eslint/no-unused-vars": warn({
-        varsIgnorePattern: "^_",
-        argsIgnorePattern: "^_",
-        caughtErrors: "all",
-      }),
+      "@typescript-eslint/no-array-constructor": OFF,
+      "@typescript-eslint/no-confusing-non-null-assertion": OFF,
+      "@typescript-eslint/no-duplicate-enum-values": OFF,
+      "@typescript-eslint/no-empty-function": OFF,
+      "@typescript-eslint/no-empty-object-type": OFF,
+      "@typescript-eslint/no-explicit-any": OFF,
+      "@typescript-eslint/no-extra-non-null-assertion": OFF,
+      "@typescript-eslint/no-inferrable-types": OFF,
+      "@typescript-eslint/no-misused-new": OFF,
+      "@typescript-eslint/no-namespace": OFF,
+      "@typescript-eslint/no-non-null-asserted-optional-chain": OFF,
+      "@typescript-eslint/no-require-imports": OFF,
+      "@typescript-eslint/no-this-alias": OFF,
+      "@typescript-eslint/no-unnecessary-type-constraint": OFF,
+      "@typescript-eslint/no-unsafe-declaration-merging": OFF,
+      "@typescript-eslint/no-unsafe-function-type": OFF,
+      "@typescript-eslint/no-unused-expressions": OFF,
+      "@typescript-eslint/no-unused-vars": OFF,
+      "@typescript-eslint/no-wrapper-object-types": OFF,
+      "@typescript-eslint/prefer-as-const": OFF,
+      "@typescript-eslint/prefer-for-of": OFF,
+      "@typescript-eslint/prefer-function-type": OFF,
+      "@typescript-eslint/prefer-namespace-keyword": OFF,
+      "@typescript-eslint/prefer-promise-reject-errors": OFF,
+      "@typescript-eslint/require-await": OFF,
+      "@typescript-eslint/triple-slash-reference": OFF,
+
       "@typescript-eslint/naming-convention": warn(
         { selector: "default", format: ["camelCase"] },
         {
