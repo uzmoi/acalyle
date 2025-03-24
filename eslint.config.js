208642fd1bebb7fdd2a45c38beb55ca893860727
uzmoi <rizzzse@gmail.com>
2025-03-24 21:03:21 +0900

    format

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,20 @@
 import { OFF, createConfig } from "@acalyle/eslint-config";
 
 export default [
-    ...createConfig({
-        tsProject: [
-            "tsconfig.json",
-            "apps/*/tsconfig.json",
-            "apps/*/tsconfig.*.json",
-            "packages/*/tsconfig.json",
-            "packages/*/tsconfig.*.json",
-        ],
-        ignores: ["apps/tauri/src-tauri/**"],
-    }),
-    {
-        files: ["packages/eslint-config/src/**"],
-        rules: {
-            "pure-module/pure-module": OFF,
-        },
+  ...createConfig({
+    tsProject: [
+      "tsconfig.json",
+      "apps/*/tsconfig.json",
+      "apps/*/tsconfig.*.json",
+      "packages/*/tsconfig.json",
+      "packages/*/tsconfig.*.json",
+    ],
+    ignores: ["apps/tauri/src-tauri/**"],
+  }),
+  {
+    files: ["packages/eslint-config/src/**"],
+    rules: {
+      "pure-module/pure-module": OFF,
     },
+  },
 ];
