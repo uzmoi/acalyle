028c4b1f6473771288f5043a21bc7ad3e0984d18
uzmoi <rizzzse@gmail.com>
2025-03-24 23:58:33 +0900

    unicornルールをいい感じにする

--- packages/eslint-config/src/recommended.ts
+++ packages/eslint-config/src/recommended.ts
@@ -3,7 +3,7 @@ import type { ESLint, Linter } from "eslint";
 import pureModule from "eslint-plugin-pure-module";
 import unicornPlugin from "eslint-plugin-unicorn";
 import { acalylePlugin } from "./acalyle";
-import { ERROR, WARN, omit, replaceWarn, warn } from "./util";
+import { ERROR, WARN, warn } from "./util";
 
 export const recommended: Linter.Config[] = [
   {
@@ -15,33 +15,36 @@ export const recommended: Linter.Config[] = [
     rules: {
       ...eslint.configs.recommended.rules,
       "acalyle/prefer-string-literal": WARN,
-      ...omit(replaceWarn(unicornPlugin.configs["flat/recommended"].rules!), [
-        "unicorn/filename-case",
-        "unicorn/import-style",
-        "unicorn/no-array-callback-reference",
-        "unicorn/no-array-reduce",
-        "unicorn/no-for-loop",
-        "unicorn/no-invalid-fetch-options",
-        "unicorn/no-length-as-slice-end",
-        "unicorn/no-magic-array-flat-depth",
-        "unicorn/no-nested-ternary",
-        "unicorn/no-null",
-        "unicorn/no-this-assignment",
-        "unicorn/prefer-at",
-        "unicorn/prefer-code-point",
-        "unicorn/prefer-global-this",
-        "unicorn/prefer-math-min-max",
-        "unicorn/prefer-module",
-        "unicorn/prefer-query-selector",
-        "unicorn/prefer-string-raw",
-        "unicorn/prefer-string-replace-all",
-        "unicorn/prefer-structured-clone",
-        "unicorn/prevent-abbreviations",
-      ]),
+      "unicorn/better-regex": WARN,
       "unicorn/consistent-destructuring": WARN,
+      "unicorn/custom-error-definition": WARN,
+      "unicorn/expiring-todo-comments": warn({
+        terms: ["todo", "fixme"],
+        allowWarningComments: false,
+      }),
+      // TODO[eslint-plugin-unicorn@>=57]: ルール追加
+      // "unicorn/no-accessor-recursion": WARN,
+      "unicorn/no-array-method-this-argument": WARN,
+      "unicorn/no-array-push-push": WARN,
+      // "unicorn/no-instanceof-builtins": WARN,
+      "unicorn/no-keyword-prefix": WARN,
+      // "unicorn/no-named-default": WARN,
+      "unicorn/no-negated-condition": WARN,
+      "unicorn/no-unnecessary-polyfills": WARN,
       "unicorn/no-unsafe-regex": ERROR,
       "unicorn/no-unused-properties": WARN,
+      "unicorn/prefer-array-find": WARN,
+      "unicorn/prefer-array-index-of": WARN,
+      "unicorn/prefer-default-parameters": WARN,
+      "unicorn/prefer-export-from": WARN,
+      "unicorn/prefer-json-parse-buffer": WARN,
+      "unicorn/prefer-keyboard-event-key": WARN,
       "unicorn/prefer-number-properties": warn({ checkInfinity: false }),
+      "unicorn/prefer-object-from-entries": WARN,
+      "unicorn/prefer-switch": WARN,
+      "unicorn/prefer-ternary": WARN,
+      "unicorn/prefer-top-level-await": WARN,
+      "unicorn/relative-url-style": WARN,
       "unicorn/template-indent": warn({
         indent: 2,
         tags: [],
