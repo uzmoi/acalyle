50ca3e0f4eb887f8ae6f12e5d5342311a0e034b6
uzmoi <rizzzse@gmail.com>
2025-03-31 22:27:23 +0900

    fix: `$book.byHandle`からbookをfetchしたときにpendingにさせる

--- apps/client/src/entities/book/model/store.ts
+++ apps/client/src/entities/book/model/store.ts
@@ -40,7 +40,9 @@ export const $book = (id: BookId): Store & PromiseLoaderExt => {
 };
 
 const handleAtomMap = new Map<BookHandle, Store>();
-const $handles = /* #__PURE__ */ map<Record<BookHandle, BookId>>({});
+const $handles = /* #__PURE__ */ map<
+  Record<BookHandle, Promise<void> | BookId | null>
+>({});
 
 // eslint-disable-next-line pure-module/pure-module
 $book.byHandle = (handle: BookHandle): Store => {
@@ -51,12 +53,21 @@ $book.byHandle = (handle: BookHandle): Store => {
   const atom = derived((get): PromiseLoaderW<Book | null> => {
     const handles = get($handles);
     const id = handles[handle];
-    if (id == null) {
-      void fetchBookByHandle(handle).then(book => {
-        if (book) {
-          $book(book.id).pending(Promise.resolve(book));
+    if (id instanceof Promise) {
+      return { status: "pending", promise: id };
+    }
+    if (id === undefined) {
+      const fetching = fetchBookByHandle(handle).then(book => {
+        if (book == null) {
+          $handles.setKey(handle, null);
+        } else {
+          $book(book.id).resolve(book);
         }
       });
+      handles[handle] = fetching;
+      return { status: "pending", promise: fetching };
+    }
+    if (id === null) {
       return { status: "fulfilled", value: null };
     }
     return get($book(id));
