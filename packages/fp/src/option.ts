75da605e46c745473d5ffe98405a792bf6d3f3a0
uzmoi <rizzzse@gmail.com>
2025-04-03 21:42:44 +0900

    format packages/fp

--- packages/fp/src/option.ts
+++ packages/fp/src/option.ts
@@ -1,62 +1,60 @@
 import { identify } from "emnorst";
 
-const none = /* #__PURE__ */ Symbol();
+const none = /* #__PURE__ */ Symbol("Option.None");
 
 type ValueOrGetter<T> = Exclude<T, () => unknown> | (() => T);
 
 export class Option<out A> {
-    static none(this: void): Option<never> {
-        return new Option<never>(none);
+  static none(this: void): Option<never> {
+    return new Option<never>(none);
+  }
+  static some<A>(this: void, value: A): Option<A> {
+    return new Option(value);
+  }
+  static from<A>(
+    this: void,
+    value: A | null | undefined,
+  ): Option<NonNullable<A>> {
+    return new Option(value ?? none);
+  }
+  private constructor(private readonly _value: A | typeof none) {}
+  isNone(): this is Option<never> {
+    return this._value === none;
+  }
+  isSome(): this is Option<A> {
+    return this._value !== none;
+  }
+  getOrThrow(): A {
+    if (this._value === none) {
+      throw new TypeError("None has no value.");
     }
-    static some<A>(this: void, value: A): Option<A> {
-        return new Option(value);
-    }
-    static from<A>(
-        this: void,
-        value: A | null | undefined,
-    ): Option<NonNullable<A>> {
-        return new Option(value ?? none);
-    }
-    private constructor(private readonly _value: A | typeof none) {}
-    isNone(): this is Option<never> {
-        return this._value === none;
-    }
-    isSome(): this is Option<A> {
-        return this._value !== none;
-    }
-    getOrThrow(): A {
-        if (this._value === none) {
-            throw new TypeError("None has no value.");
-        }
-        return this._value;
-    }
-    getOrElse<B>(value: ValueOrGetter<B>): A | B {
-        return (
-            this._value === none ?
-                typeof value === "function" ?
-                    (value as () => B)()
-                :   value
-            :   this._value
-        );
-    }
-    orElse<B>(option: ValueOrGetter<Option<B>>): Option<A | B> {
-        return (
-            this._value === none ?
-                typeof option === "function" ?
-                    option()
-                :   option
-            :   this
-        );
-    }
-    map<B>(fn: (value: A) => B): Option<B> {
-        return this._value === none ?
-                Option.none()
-            :   Option.some(fn(this._value));
-    }
-    flatMap<B>(fn: (value: A) => Option<B>): Option<B> {
-        return this._value === none ? Option.none() : fn(this._value);
-    }
-    flat<B>(this: Option<Option<B>>): Option<B> {
-        return this.flatMap(identify);
-    }
+    return this._value;
+  }
+  getOrElse<B>(value: ValueOrGetter<B>): A | B {
+    return (
+      this._value === none ?
+        typeof value === "function" ?
+          (value as () => B)()
+        : value
+      : this._value
+    );
+  }
+  orElse<B>(option: ValueOrGetter<Option<B>>): Option<A | B> {
+    return (
+      this._value === none ?
+        typeof option === "function" ?
+          option()
+        : option
+      : this
+    );
+  }
+  map<B>(fn: (value: A) => B): Option<B> {
+    return this._value === none ? Option.none() : Option.some(fn(this._value));
+  }
+  flatMap<B>(fn: (value: A) => Option<B>): Option<B> {
+    return this._value === none ? Option.none() : fn(this._value);
+  }
+  flat<B>(this: Option<Option<B>>): Option<B> {
+    return this.flatMap(identify);
+  }
 }
--- packages/fp/tsup.config.ts
+++ packages/fp/tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/index.ts"],
-    format: "esm",
-    minifySyntax: true,
-    esbuildOptions(options) {
-        options.mangleProps = /^_[^_]/;
-    },
-    sourcemap: true,
-    dts: true,
+  entry: ["src/index.ts"],
+  format: "esm",
+  minifySyntax: true,
+  esbuildOptions(options) {
+    options.mangleProps = /^_[^_]/;
+  },
+  sourcemap: true,
+  dts: true,
 });
