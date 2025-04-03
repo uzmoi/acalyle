be49426a9ae2b48839a4b122c81c5734a28c1958
uzmoi <rizzzse@gmail.com>
2025-04-04 00:23:06 +0900

    targetをESNextに

--- packages/fp/tsup.config.ts
+++ packages/fp/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: "esm",
+  target: "esnext",
   minifySyntax: true,
   esbuildOptions(options) {
     options.mangleProps = /^_[^_]/;
