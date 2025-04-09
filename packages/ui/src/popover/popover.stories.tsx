fcd70ec00a617efc8aca4055cab2bfad9a9d5ab7
uzmoi <rizzzse@gmail.com>
2025-04-09 19:41:57 +0900

    add story

--- packages/ui/src/popover/popover.stories.tsx
+++ packages/ui/src/popover/popover.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { fn } from "@storybook/test";
+import { expect, fn, userEvent } from "@storybook/test";
 import { Button } from "../control";
 import {
   Popover,
@@ -32,3 +32,19 @@ export default {
 type Story = StoryObj<PopoverProps & PopoverContentProps>;
 
 export const Default: Story = {};
+
+export const Opened: Story = {
+  async play({ canvas, args }) {
+    const button = canvas.getByRole("button");
+    await userEvent.click(button);
+    await expect(args.onOpen).toHaveBeenCalled();
+
+    const content = await canvas.findByText("content");
+    await expect(content).toBeVisible();
+
+    await userEvent.click(content);
+    await expect(args.onClose).not.toHaveBeenCalled();
+
+    await expect(content).toBeVisible();
+  },
+};
