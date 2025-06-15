40f602e2f871db0f8596703ef5ada8acf0e95c49
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-06-16 00:10:09 +0900

    refactor

--- apps/client/src/features/search-notes/model/query.ts
+++ apps/client/src/features/search-notes/model/query.ts
@@ -7,13 +7,17 @@ export type QueryItem = { type: "word"; value: string };
 export const parseQuery = (query: string): Query => {
   const items = query
     .match(/"(?:\\.|[^\\])*"(?!\P{White_Space})|\P{White_Space}+/gv)
-    ?.map(word =>
-      word.length >= 2 && word.startsWith('"') && word.endsWith('"') ?
-        word.slice(1, -1).replaceAll(/\\(.)/gv, "$1")
-      : word,
-    )
-    .filter(Boolean)
-    .map<QueryItem>(word => ({ type: "word", value: word }));
+    ?.map<QueryItem>(part => {
+      // quote
+      if (part.length >= 2 && part.startsWith('"') && part.endsWith('"')) {
+        const value = part.slice(1, -1).replaceAll(/\\(.)/gv, "$1");
+
+        return { type: "word", value };
+      }
+
+      return { type: "word", value: part };
+    })
+    .filter(item => item.value !== "");
 
   return { items: items ?? [] };
 };
