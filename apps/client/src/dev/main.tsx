fcc01fe17266e8bc47edee984e478deaf5969000
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-09-03 21:58:02 +0900

    fix

--- apps/client/src/dev/main.tsx
+++ apps/client/src/dev/main.tsx
@@ -8,18 +8,16 @@ import { globalStyle } from "@acalyle/css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "~/app/app";
-import "./logger";
-import "./location";
 
 globalStyle(":root, body, #app", {
-    height: "100%",
+  height: "100%",
 });
 
 const appEl = document.getElementById("app");
 if (appEl != null) {
-    createRoot(appEl).render(
-        <StrictMode>
-            <App />
-        </StrictMode>,
-    );
+  createRoot(appEl).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
 }
