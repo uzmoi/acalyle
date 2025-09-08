1a5e9f4880aeb5934e2da1e2a379444b7389ad9a
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-09-08 22:50:56 +0900

    refactor

--- apps/client/src/pages/book-list/ui/thumbnail.tsx
+++ apps/client/src/pages/book-list/ui/thumbnail.tsx
@@ -1,13 +1,13 @@
 import { cx, style } from "@acalyle/css";
-import { acalyle } from "~/app/main";
 import { theme } from "~/theme";
+import { resolveResource } from "../model";
 
 export const BookThumbnail: React.FC<{
   src: string;
   className?: string;
 }> = ({ src, className }) => {
   const divClassName = cx(
-    ":uno: w-24 h-24 overflow-hidden",
+    ":uno: size-24 overflow-hidden",
     style({ borderRadius: theme("bookOverview-round") }),
     className,
   );
@@ -21,9 +21,10 @@ export const BookThumbnail: React.FC<{
   return (
     <div className={divClassName}>
       <img
-        src={acalyle.net.resolveResource(src).href}
+        src={resolveResource(src).href}
         alt="book thumbnail"
-        className=":uno: w-full"
+        width={96}
+        height={96}
       />
     </div>
   );
