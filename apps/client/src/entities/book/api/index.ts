ecc3e2c9ce7a228a21e76981503958ea170b0ff6
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-09-11 01:47:44 +0900

    refactor fetchBook and fetchBookByHandle

--- apps/client/src/entities/book/api/index.ts
+++ apps/client/src/entities/book/api/index.ts
@@ -3,23 +3,34 @@ import { type ID, gql, type GqlFnError } from "~/shared/graphql";
 import type { Book, BookHandle, BookId } from "../model";
 import BookQuery from "./book.graphql";
 
+interface GqlBook {
+  id: ID;
+  handle: string | null;
+  title: string;
+  description: string;
+  thumbnail: string;
+  tags: string[];
+}
+
+const adjustType = (book: GqlBook | null): Book | null => {
+  if (book == null) return null;
+
+  return {
+    id: book.id as string as BookId,
+    handle: book.handle as BookHandle | null,
+    title: book.title,
+    description: book.description,
+    thumbnail: book.thumbnail,
+    tags: book.tags,
+  };
+};
+
 export const fetchBook = async (
   id: BookId,
 ): Promise<Result<Book | null, GqlFnError>> => {
   const result = await gql(BookQuery, { bookId: id as string as ID });
 
-  return result.map(({ book }) => {
-    if (book == null) return null;
-
-    return {
-      id: book.id as string as BookId,
-      handle: book.handle as BookHandle | null,
-      title: book.title,
-      description: book.description,
-      thumbnail: book.thumbnail,
-      tags: book.tags,
-    };
-  });
+  return result.map(({ book }) => adjustType(book));
 };
 
 export const fetchBookByHandle = async (
@@ -27,16 +38,5 @@ export const fetchBookByHandle = async (
 ): Promise<Result<Book | null, GqlFnError>> => {
   const result = await gql(BookQuery, { handle });
 
-  return result.map(({ book }) => {
-    if (book == null) return null;
-
-    return {
-      id: book.id as string as BookId,
-      handle: book.handle as BookHandle | null,
-      title: book.title,
-      description: book.description,
-      thumbnail: book.thumbnail,
-      tags: book.tags,
-    };
-  });
+  return result.map(({ book }) => adjustType(book));
 };
