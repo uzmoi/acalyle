1644a879baf00a1e3b9e46d24baff730e4eb1a3c
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-10-01 02:53:47 +0900

    refactor tag style

--- apps/client/src/entities/note/ui/tag.tsx
+++ apps/client/src/entities/note/ui/tag.tsx
@@ -1,24 +1,21 @@
 import type { NoteTag } from "@acalyle/core";
-import { style } from "@acalyle/css";
-import { vars, theme } from "@acalyle/ui";
+import { cx, style } from "@acalyle/css";
+import { theme, vars } from "@acalyle/ui";
 
 export const Tag: React.FC<{
   tag: NoteTag;
 }> = ({ tag }) => {
   return (
     <span
-      className={style({
-        display: "inline-block",
-        padding: "0.125rem 0.25rem",
-        lineHeight: 1,
-        fontSize: "0.75em",
-        fontFamily: vars.font.mono,
-        color: theme("tag-text"),
-        background: theme("tag-bg"),
-        border: "1px solid",
-        borderColor: theme("tag-outline"),
-        borderRadius: vars.radius.block,
-      })}
+      className={cx(
+        ":uno: inline-block b rounded b-solid px-1 py-0.5 text-xs",
+        style({
+          fontFamily: vars.font.mono,
+          color: theme("tag-text"),
+          background: theme("tag-bg"),
+          borderColor: theme("tag-outline"),
+        }),
+      )}
       data-tag-type={tag.type}
     >
       <span>{tag.symbol}</span>
