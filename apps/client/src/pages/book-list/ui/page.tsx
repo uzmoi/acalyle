6b9561eb59fdfc6bd58d669c0e6e2a36e3490a2f
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-11-18 21:28:37 +0900

    リンクボタン化

--- apps/client/src/pages/book-list/ui/page.tsx
+++ apps/client/src/pages/book-list/ui/page.tsx
@@ -29,7 +29,7 @@ export const BookListPage: React.FC<{
     <main className=":uno: mx-auto max-w-screen-xl px-8 py-4">
       <div className=":uno: mb-6 flex flex-wrap items-center gap-x-4 gap-y-2">
         <SearchBar initialQuery={query} />
-        <Link to="/books/new">
+        <Link to="/books/new" button>
           <BiBookAdd />
           <span className=":uno: ml-1">New</span>
         </Link>
