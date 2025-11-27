d41415e5ae3ef65499e7b674f1c7ab5bf1bfe3a2
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-11-28 01:35:13 +0900

    BookPagesHeaderにサムネイルを追加

--- apps/client/src/widgets/book-pages-header/ui/header.tsx
+++ apps/client/src/widgets/book-pages-header/ui/header.tsx
@@ -1,21 +1,27 @@
-import { type Book, bookRefOf } from "#entities/book";
+import { type Book, BookThumbnail, bookRefOf } from "#entities/book";
 import { Link } from "#shared/ui";
 
 export const BookPagesHeader: React.FC<{
   book: Book;
 }> = ({ book }) => {
   return (
-    <header>
-      <h2 className=":uno: text-2xl">
-        <Link
-          to="/books/$book-ref"
-          params={{ "book-ref": bookRefOf(book) }}
-          className=":uno: decoration-none"
-        >
-          {book.title}
-        </Link>
-      </h2>
-      <p className=":uno: text-xs text-gray">{book.description}</p>
+    <header className=":uno: flex gap-4 items-center">
+      <BookThumbnail
+        thumbnail={book.thumbnail}
+        className=":uno: size-14 rounded-2"
+      />
+      <div>
+        <h2 className=":uno: text-2xl">
+          <Link
+            to="/books/$book-ref"
+            params={{ "book-ref": bookRefOf(book) }}
+            className=":uno: decoration-none"
+          >
+            {book.title}
+          </Link>
+        </h2>
+        <p className=":uno: text-xs text-gray">{book.description}</p>
+      </div>
     </header>
   );
 };
