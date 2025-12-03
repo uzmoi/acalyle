a66f23371bb5b64148b44ee223518bdbf691bac8
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-12-04 00:46:50 +0900

    refactor: createRouterを別ファイルに移動

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,20 +1,9 @@
 import { cx, style } from "@acalyle/css";
 import { theme, vars } from "@acalyle/ui";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { RouterProvider } from "@tanstack/react-router";
 import { QuickModalContainer } from "~/features/modal";
-import { routeTree } from "./routeTree.gen";
+import { router } from "./router";
 import { defaultThemeClassName } from "./theme";
-
-const router = /* #__PURE__ */ createRouter({
-  routeTree,
-  defaultPreload: "intent",
-});
-
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
 
 export const App: React.FC = () => {
   return (
